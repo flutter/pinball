7edb174f1266d8881232a508f2f6648893cccc55
Erick Zanardo <erickzanardoo@gmail.com>
2022-02-28 18:44:15 -0300

    adding FlameBloc to the PinballGame

--- lib/game/pinball_game.dart
+++ lib/game/pinball_game.dart
@@ -1,3 +1,4 @@
+import 'package:flame_bloc/flame_bloc.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
 
-class PinballGame extends Forge2DGame {}
+class PinballGame extends Forge2DGame with FlameBloc {}
