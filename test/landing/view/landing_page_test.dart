ad16d8a2564fb64bdce41bf82377b7e3c49a3299
Erick Zanardo <erickzanardoo@gmail.com>
2022-02-28 11:57:06 -0300

    fixing landing page test

--- test/landing/view/landing_page_test.dart
+++ test/landing/view/landing_page_test.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pinball/game/game.dart';
+import 'package:mockingjay/mockingjay.dart';
 import 'package:pinball/landing/landing.dart';
 
 import '../../helpers/helpers.dart';
@@ -16,17 +14,20 @@ void main() {
 
     testWidgets('tapping on TextButton navigates to PinballGamePage',
         (tester) async {
-      // TODO(erickzanardo): Make test pass.
-      await tester.pumpApp(const LandingPage());
+      final navigator = MockNavigator();
+      when(() => navigator.push<void>(any())).thenAnswer((_) async {});
+
+      await tester.pumpApp(
+        const LandingPage(),
+        navigator: navigator,
+      );
       await tester.tap(
         find.byType(
           TextButton,
         ),
       );
-      await tester.pumpAndSettle();
 
-      expect(find.byType(PinballGamePage), findsOneWidget);
-      expect(find.byType(LandingPage), findsNothing);
+      verify(() => navigator.push<void>(any())).called(1);
     });
   });
 }
