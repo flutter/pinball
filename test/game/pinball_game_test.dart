f5b14436db7729b829a2678506e802ad6eb93021
alestiago <dev@alestiago.com>
2022-03-02 19:23:15 +0000

    refactor: fixed analyzer warnings

--- test/game/pinball_game_test.dart
+++ test/game/pinball_game_test.dart
@@ -1,10 +1,9 @@
-import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('PinballGame', () {
-    // TODO(alestiago): test if [PinballGame] registers [BallScorePointsCallback]
-    // once the following issue is resolved:
+    // TODO(alestiago): test if [PinballGame] registers
+    // [BallScorePointsCallback] once the following issue is resolved:
     // https://github.com/flame-engine/flame/issues/1416
   });
 }
