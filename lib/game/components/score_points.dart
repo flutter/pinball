5f04e23932230b00304aa0a6a5fc6997abb97747
alestiago <dev@alestiago.com>
2022-03-02 18:39:29 +0000

    docs: included doc comments

--- lib/game/components/score_points.dart
+++ lib/game/components/score_points.dart
@@ -3,10 +3,16 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:pinball/game/game.dart';
 
+/// {@template score_points}
+/// Specifies the amount of points received on [Ball] collision.
+/// {@endtemplate}
 mixin ScorePoints {
+  /// {@macro score_points}
   int get points;
 }
 
+/// [ContactCallback] that adds points to the score when a [Ball] collides with
+/// a [BodyComponent] that implements [ScorePoints].
 class BallScorePointsCallback extends ContactCallback<Ball, ScorePoints> {
   @override
   void begin(
