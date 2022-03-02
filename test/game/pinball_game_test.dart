5900562a0518c58229b62fe046d59f77da895840
alestiago <dev@alestiago.com>
2022-03-02 19:18:06 +0000

    feat: registered BallScorePointsCallback in PinballGame

---
+++ test/game/pinball_game_test.dart
@@ -0,0 +1,10 @@
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('PinballGame', () {
+    // TODO(alestiago): test if [PinballGame] registers [BallScorePointsCallback]
+    // once the following issue is resolved:
+    // https://github.com/flame-engine/flame/issues/1416
+  });
+}
