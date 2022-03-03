d6686b738a3adc61856bce1b1b8e0896c0ba02cd
Allison Ryan <allisonryan0002@gmail.com>
2022-03-03 08:11:14 -0600

    feat: add themes

---
+++ lib/character_themes/sparky_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pinball/character_themes/character_themes.dart';
+
+class SparkyTheme extends CharacterTheme {
+  const SparkyTheme();
+
+  @override
+  Color get ballColor => Colors.orange;
+}
