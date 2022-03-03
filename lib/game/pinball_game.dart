64d4677b3f5077f76e12cd1da23b56e0cbc8975d
Allison Ryan <allisonryan0002@gmail.com>
2022-03-03 14:20:03 -0600

    chore: remove modifications to game

--- lib/game/pinball_game.dart
+++ lib/game/pinball_game.dart
@@ -1,12 +1,10 @@
 import 'package:flame_bloc/flame_bloc.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
-
 import 'package:pinball/game/game.dart';
 
 class PinballGame extends Forge2DGame with FlameBloc {
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
     addContactCallback(BallScorePointsCallback());
   }
 }
