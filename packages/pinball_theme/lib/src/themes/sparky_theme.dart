f0db7c720d8c1e848b8ef7a4182b0876d7a10928
Allison Ryan <allisonryan0002@gmail.com>
2022-03-04 11:16:13 -0600

    docs: character themes update

--- packages/pinball_theme/lib/src/themes/sparky_theme.dart
+++ packages/pinball_theme/lib/src/themes/sparky_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pinball_theme/pinball_theme.dart';
 
 /// {@template sparky_theme}
-/// Class defining Sparky theme assets and attributes.
+/// Defines Sparky theme assets and attributes.
 /// {@endtemplate}
 class SparkyTheme extends PinballTheme {
   /// {@macro sparky_theme}
