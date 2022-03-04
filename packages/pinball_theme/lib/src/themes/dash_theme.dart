ca6c190ee5709f215a09a54392aa0cbb6b17f9af
Allison Ryan <allisonryan0002@gmail.com>
2022-03-04 11:16:13 -0600

    docs: character themes update

--- packages/pinball_theme/lib/src/themes/dash_theme.dart
+++ packages/pinball_theme/lib/src/themes/dash_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pinball_theme/pinball_theme.dart';
 
 /// {@template dash_theme}
-/// Class defining Dash theme assets and attributes.
+/// Defines Dash theme assets and attributes.
 /// {@endtemplate}
 class DashTheme extends PinballTheme {
   /// {@macro dash_theme}
--- packages/pinball_theme/lib/src/themes/dino_theme.dart
+++ packages/pinball_theme/lib/src/themes/dino_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pinball_theme/pinball_theme.dart';
 
 /// {@template dino_theme}
-/// Class defining Dino theme assets and attributes.
+/// Defines Dino theme assets and attributes.
 /// {@endtemplate}
 class DinoTheme extends PinballTheme {
   /// {@macro dino_theme}
