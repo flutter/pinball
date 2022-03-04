6c4090d0922ef481de1aa923f321a8adcdfc6c0a
alestiago <dev@alestiago.com>
2022-03-04 12:06:55 +0000

    chore: fixed white space difference

--- lib/game/components/anchor.dart
+++ lib/game/components/anchor.dart
@@ -1,7 +1,7 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 
 /// {@template anchor}
-/// Non visual [BodyComponent] used to hold a [BodyType.dynamic] in [Joint]s 
+/// Non visual [BodyComponent] used to hold a [BodyType.dynamic] in [Joint]s
 /// with this [BodyType.static].
 ///
 /// It is recommended to [_position] the anchor first and then use the body
