293ce9256c832fb471681f6e9edaa740d91203ac
Allison Ryan <allisonryan0002@gmail.com>
2022-03-10 16:13:25 -0600

    docs: small change

--- test/helpers/extensions.dart
+++ test/helpers/extensions.dart
@@ -3,7 +3,7 @@ import 'package:pinball_theme/pinball_theme.dart';
 
 /// [PinballGame] extension to reduce boilerplate in tests.
 extension PinballGameTest on PinballGame {
-  /// [PinballGame] with default [PinballTheme].
+  /// Create [PinballGame] with default [PinballTheme].
   static PinballGame create() => PinballGame(
         theme: const PinballTheme(
           characterTheme: DashTheme(),
