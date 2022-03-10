7a59045e436664d31bef4036c0e733b2d43ae6d3
alestiago <dev@alestiago.com>
2022-03-10 09:05:31 +0000

    feat: created sling-short.dart

---
+++ lib/game/components/sling-shot.dart
@@ -0,0 +1,9 @@
+import 'package:flame_forge2d/flame_forge2d.dart';
+
+class SlingShot extends BodyComponent {
+  @override
+  Body createBody() {
+    // TODO: implement createBody
+    throw UnimplementedError();
+  }
+}
