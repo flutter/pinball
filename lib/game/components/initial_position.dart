fee19bb16b7e988357dc4dff57fb4bd6c9c35585
alestiago <dev@alestiago.com>
2022-03-14 09:35:13 +0000

    docs: included doc comments

--- lib/game/components/initial_position.dart
+++ lib/game/components/initial_position.dart
@@ -1,6 +1,9 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 
+/// Forces a given [BodyComponent] to position their [body] to an
+/// [initialPosition].
 mixin InitialPosition on BodyComponent {
+  /// The initial position of the [body].
   late final Vector2 initialPosition;
 
   @override
