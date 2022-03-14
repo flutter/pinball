cc36971eaf1d9aa0245e80567ca4bf479078561c
Rui Miguel Alonso <ruiskas@gmail.com>
2022-03-14 18:53:24 +0100

    Update lib/game/components/jetpack_ramp.dart
    Co-authored-by: Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>

--- lib/game/components/jetpack_ramp.dart
+++ lib/game/components/jetpack_ramp.dart
@@ -6,9 +6,8 @@ import 'package:pinball/game/game.dart';
 /// {@template jetpack_ramp}
 /// Represents the upper left blue ramp for the game.
 ///
-/// Group of [Component]s composed by a [Pathway.arc] as the ramp, and two
-/// [JetpackRampArea] at the entrance and exit of the ramp, to detect when
-/// a ball gets into/out of the ramp.
+/// Composed of a [Pathway.arc] defining the ramp, and two
+/// [JetpackRampArea]s at the entrance and exit of the ramp.
 /// {@endtemplate}
 class JetpackRamp extends PositionComponent with HasGameRef<PinballGame> {
   /// {@macro jetpack_ramp}
