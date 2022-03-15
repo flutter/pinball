f303fc4850f9699ae1a45a00d1123656fd8716aa
alestiago <dev@alestiago.com>
2022-03-15 08:54:56 +0000

    docs: improved doc comment

--- lib/game/components/board_side.dart
+++ lib/game/components/board_side.dart
@@ -22,6 +22,6 @@ extension BoardSideX on BoardSide {
 
   /// Direction of the [BoardSide].
   ///
-  /// Represents the line along the [BoardSide] moves.
+  /// Represents the line which the [BoardSide] moves.
   int get direction => isLeft ? -1 : 1;
 }
