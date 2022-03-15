51c9a4dc65eb2aab358360b202719c1f7e81dbd6
RuiAlonso <rui.alonso@verygood.ventures>
2022-03-15 14:33:34 +0100

    feat: navigate to leaderboard from gameover dialog

--- lib/game/view/widgets/game_over_dialog.dart
+++ lib/game/view/widgets/game_over_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pinball/game/game.dart';
+import 'package:pinball/l10n/l10n.dart';
+import 'package:pinball/leaderboard/leader_board.dart';
 
 /// {@template game_over_dialog}
 /// [Dialog] displayed when the [PinballGame] is over.
@@ -10,12 +12,25 @@ class GameOverDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Dialog(
+    final l10n = context.l10n;
+
+    return Dialog(
       child: SizedBox(
         width: 200,
         height: 200,
         child: Center(
-          child: Text('Game Over'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(l10n.gameOver),
+              TextButton(
+                onPressed: () => Navigator.of(context).push<void>(
+                  LeaderBoardPage.route(),
+                ),
+                child: Text(l10n.leadersBoard),
+              ),
+            ],
+          ),
         ),
       ),
     );
