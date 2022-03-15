5b4f5f8794b26bde83254fa007332ab7c460c3ba
alestiago <dev@alestiago.com>
2022-03-15 12:40:37 +0000

    feat: included generics

--- lib/game/components/initial_position.dart
+++ lib/game/components/initial_position.dart
@@ -2,7 +2,7 @@ import 'package:flame_forge2d/flame_forge2d.dart';
 
 /// Forces a given [BodyComponent] to position their [body] to an
 /// [initialPosition].
-mixin InitialPosition on BodyComponent {
+mixin InitialPosition<T extends Forge2DGame> on BodyComponent<T> {
   /// The initial position of the [body].
   late final Vector2 initialPosition;
 
