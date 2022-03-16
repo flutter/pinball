377d5f51bb115de97df8f864ec9577c0d7f96457
RuiAlonso <rui.alonso@verygood.ventures>
2022-03-16 19:39:40 +0100

    feat: added empty repository

--- lib/leaderboard/bloc/leaderboard_bloc.dart
+++ lib/leaderboard/bloc/leaderboard_bloc.dart
@@ -7,44 +7,27 @@ import 'package:pinball_theme/pinball_theme.dart';
 part 'leaderboard_event.dart';
 part 'leaderboard_state.dart';
 
+class LeaderboardRepository {
+  Future<List<Competitor>> fetchRanking() {
+    return Future.value([]);
+  }
+}
+
 class LeaderboardBloc extends Bloc<LeaderboardEvent, LeaderboardState> {
-  LeaderboardBloc() : super(const LeaderboardState()) {
+  LeaderboardBloc(this._leaderboardRepository)
+      : super(const LeaderboardState()) {
     on<LeaderboardRequested>(_onLeaderboardRequested);
   }
+
+  final LeaderboardRepository _leaderboardRepository;
 
   FutureOr<void> _onLeaderboardRequested(
     LeaderboardRequested event,
     Emitter<LeaderboardState> emit,
-  ) {
+  ) async {
     emit(state.copyWith(status: LeaderboardStatus.loading));
     try {
-      const ranking = <Competitor>[
-        Competitor(
-          rank: 1,
-          characterTheme: DashTheme(),
-          initials: 'ABC',
-          score: 100,
-        ),
-        Competitor(
-          rank: 2,
-          characterTheme: SparkyTheme(),
-          initials: 'DEF',
-          score: 200,
-        ),
-        Competitor(
-          rank: 3,
-          characterTheme: AndroidTheme(),
-          initials: 'GHI',
-          score: 300,
-        ),
-        Competitor(
-          rank: 4,
-          characterTheme: DinoTheme(),
-          initials: 'JKL',
-          score: 400,
-        ),
-      ];
-
+      final ranking = await _leaderboardRepository.fetchRanking();
       emit(
         state.copyWith(
           status: LeaderboardStatus.success,
