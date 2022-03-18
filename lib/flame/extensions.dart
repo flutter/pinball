da1e22792285f49af21f28dc958ab5329586c3ab
Erick Zanardo <erickzanardoo@gmail.com>
2022-03-18 17:04:26 -0300

    fix: lint

--- lib/flame/extensions.dart
+++ lib/flame/extensions.dart
@@ -1,6 +1,5 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame_forge2d/contact_callbacks.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/foundation.dart';
 
