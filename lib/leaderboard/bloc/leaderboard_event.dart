84636b0c102d6cc2593a6b9e31aa5235a1fac884
RuiAlonso <rui.alonso@verygood.ventures>
2022-03-18 09:10:24 +0100

    doc: completed doc

--- lib/leaderboard/bloc/leaderboard_event.dart
+++ lib/leaderboard/bloc/leaderboard_event.dart
@@ -1,20 +1,29 @@
 part of 'leaderboard_bloc.dart';
 
+/// {@template leaderboard_event}
 /// Represents the events available for [LeaderboardBloc].
+/// {endtemplate}
 abstract class LeaderboardEvent extends Equatable {
+  /// {@macro leaderboard_event}
   const LeaderboardEvent();
 
   @override
   List<Object?> get props => [];
 }
 
+/// {@template top_10_fetched}
 /// Request the top 10 from leaderboard.
+/// {endtemplate}
 class Top10Fetched extends LeaderboardEvent {
+  /// {@macro top_10_fetched}
   const Top10Fetched();
 }
 
+/// {@template leaderboard_entry_added}
 /// Insert new entry at leaderboard when user ends game.
+/// {endtemplate}
 class LeaderboardEntryAdded extends LeaderboardEvent {
+  /// {@macro leaderboard_entry_added}
   const LeaderboardEntryAdded({required this.entry});
 
   /// Current entry to persist at leaderboard.
