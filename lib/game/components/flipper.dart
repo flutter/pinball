6a6caf1abcdaf41866ee03700e6fdeee5566ab63
alestiago <dev@alestiago.com>
2022-03-21 11:20:37 +0000

    docs: renamed Anchor reference

--- lib/game/components/flipper.dart
+++ lib/game/components/flipper.dart
@@ -216,7 +216,7 @@ class Flipper extends BodyComponent with KeyboardHandler, InitialPosition {
 }
 
 /// {@template flipper_anchor}
-/// [Anchor] positioned at the end of a [Flipper].
+/// [JointAnchor] positioned at the end of a [Flipper].
 ///
 /// The end of a [Flipper] depends on its [Flipper.side].
 /// {@endtemplate}
