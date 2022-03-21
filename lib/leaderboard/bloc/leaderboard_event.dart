15ce26098d13c27077203e6834e4373c65668fe7
Rui Miguel Alonso <ruiskas@gmail.com>
2022-03-21 10:19:57 +0100

    Update lib/leaderboard/bloc/leaderboard_event.dart
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

--- lib/leaderboard/bloc/leaderboard_event.dart
+++ lib/leaderboard/bloc/leaderboard_event.dart
@@ -28,7 +28,7 @@ class LeaderboardEntryAdded extends LeaderboardEvent {
   /// {@macro leaderboard_entry_added}
   const LeaderboardEntryAdded({required this.entry});
 
-  /// Current entry to persist at leaderboard.
+  /// [LeadboardEntry] to be written to the remote storage.
   final LeaderboardEntry entry;
 
   @override
