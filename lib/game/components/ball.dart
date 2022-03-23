ff57b010b1ffd3d345dcf9c1f7f638467e72544a
RuiAlonso <rui.alonso@verygood.ventures>
2022-03-23 17:58:41 +0100

    refactor: changed ball with Elevated

--- lib/game/components/ball.dart
+++ lib/game/components/ball.dart
@@ -7,7 +7,8 @@ import 'package:pinball/gen/assets.gen.dart';
 /// A solid, [BodyType.dynamic] sphere that rolls and bounces along the
 /// [PinballGame].
 /// {@endtemplate}
-class Ball extends BodyComponent<PinballGame> with InitialPosition, Layered {
+class Ball extends BodyComponent<PinballGame>
+    with InitialPosition, Layered, Elevated {
   /// {@macro ball}
   Ball() {
     // TODO(ruimiguel): while developing Ball can be launched by clicking mouse,
