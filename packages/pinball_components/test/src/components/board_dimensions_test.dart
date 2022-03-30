c5df602b71a39d85d489895909036aafa7662085
Allison Ryan <allisonryan0002@gmail.com>
2022-03-30 16:19:41 -0500

    test: board dimensions

---
+++ packages/pinball_components/test/src/components/board_dimensions_test.dart
@@ -0,0 +1,23 @@
+import 'package:flame/extensions.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pinball_components/pinball_components.dart';
+
+void main() {
+  group('BoardDimensions', () {
+    test('has size', () {
+      expect(BoardDimensions.size, equals(Vector2(101.6, 143.8)));
+    });
+
+    test('has bounds', () {
+      expect(BoardDimensions.bounds, isNotNull);
+    });
+
+    test('has perspectiveAngle', () {
+      expect(BoardDimensions.perspectiveAngle, isNotNull);
+    });
+
+    test('has perspectiveShrinkFactor', () {
+      expect(BoardDimensions.perspectiveShrinkFactor, equals(0.63));
+    });
+  });
+}
