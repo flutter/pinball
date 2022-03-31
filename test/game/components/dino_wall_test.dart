beef4a2fbd5898404176afc24b85969626dbb711
RuiAlonso <rui.alonso@verygood.ventures>
2022-03-31 21:22:29 +0200

    test: removed unused import

--- test/game/components/dino_wall_test.dart
+++ test/game/components/dino_wall_test.dart
@@ -1,9 +1,7 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:pinball/game/components/components.dart';
-import 'package:pinball_components/pinball_components.dart';
 
 import '../../helpers/helpers.dart';
 
