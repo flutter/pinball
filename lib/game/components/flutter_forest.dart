e5c37089522d45dc25f43f2336ac0062bd7c75c9
Alejandro Santiago <dev@alestiago.com>
2022-04-01 09:35:03 +0100

    feat: implemented DashBumper assets (#119)
    * chore: rebase
    
    * chore: rebase
    
    * chore: rebase
    
    * docs: removed extra paragraph
    
    * fix: removed checks
    
    * refactor: removed ephemeral state
    
    * refactor: uncommented code
    
    * feat: automatically sized Sprites
    
    * docs: included TODO comment
    
    * refactor: corrected typo
    
    * docs: updated doc comment
    
    * feat: adjusted tests
    
    * chore: rebase
    
    * feat: included mustCallSuper
    
    * feat: implemented FlutterForest controllers
    
    * feat: improved tests
    
    * fix: analyzer
    
    * refactor: removed unneccessary mock
    
    * feat: include DashNestBumper tests
    
    * docs: improved doc comment
    
    * refactor: fixed test name grammar
    
    * fix: fixed test

--- lib/game/components/flutter_forest.dart
+++ lib/game/components/flutter_forest.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: avoid_renaming_method_parameters
 
-import 'dart:math' as math;
-
 import 'package:flame/components.dart';
 import 'package:flame_bloc/flame_bloc.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:pinball/flame/flame.dart';
 import 'package:pinball/game/game.dart';
 import 'package:pinball_components/pinball_components.dart';
 
@@ -16,10 +15,47 @@ import 'package:pinball_components/pinball_components.dart';
 /// When all [DashNestBumper]s are hit at least once, the [GameBonus.dashNest]
 /// is awarded, and the [BigDashNestBumper] releases a new [Ball].
 /// {@endtemplate}
-// TODO(alestiago): Make a [Blueprint] once nesting [Blueprint] is implemented.
-class FlutterForest extends Component
-    with HasGameRef<PinballGame>, BlocComponent<GameBloc, GameState> {
+// TODO(alestiago): Make a [Blueprint] once [Blueprint] inherits from
+// [Component].
+class FlutterForest extends Component with Controls<_FlutterForestController> {
   /// {@macro flutter_forest}
+  FlutterForest() {
+    controller = _FlutterForestController(this);
+  }
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    final signPost = FlutterSignPost()..initialPosition = Vector2(8.35, 58.3);
+
+    final bigNest = _ControlledBigDashNestBumper(
+      id: 'big_nest_bumper',
+    )..initialPosition = Vector2(18.55, 59.35);
+    final smallLeftNest = _ControlledSmallDashNestBumper.a(
+      id: 'small_nest_bumper_a',
+    )..initialPosition = Vector2(8.95, 51.95);
+    final smallRightNest = _ControlledSmallDashNestBumper.b(
+      id: 'small_nest_bumper_b',
+    )..initialPosition = Vector2(23.3, 46.75);
+
+    await addAll([
+      signPost,
+      smallLeftNest,
+      smallRightNest,
+      bigNest,
+    ]);
+  }
+}
+
+class _FlutterForestController extends ComponentController<FlutterForest>
+    with BlocComponent<GameBloc, GameState>, HasGameRef<PinballGame> {
+  _FlutterForestController(FlutterForest flutterForest) : super(flutterForest);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    gameRef.addContactCallback(_ControlledDashNestBumperBallContactCallback());
+  }
 
   @override
   bool listenWhen(GameState? previousState, GameState newState) {
@@ -32,117 +68,90 @@ class FlutterForest extends Component
   void onNewState(GameState state) {
     super.onNewState(state);
 
-    add(
-      ControlledBall.bonus(
-        theme: gameRef.theme,
-      )..initialPosition = Vector2(17.2, 52.7),
+    component.add(
+      ControlledBall.bonus(theme: gameRef.theme)
+        ..initialPosition = Vector2(17.2, 52.7),
     );
   }
-
-  @override
-  Future<void> onLoad() async {
-    gameRef.addContactCallback(DashNestBumperBallContactCallback());
-
-    final signPost = FlutterSignPost()..initialPosition = Vector2(8.35, 58.3);
-
-    // TODO(alestiago): adjust positioning once sprites are added.
-    final smallLeftNest = SmallDashNestBumper(id: 'small_left_nest')
-      ..initialPosition = Vector2(8.95, 51.95);
-    final smallRightNest = SmallDashNestBumper(id: 'small_right_nest')
-      ..initialPosition = Vector2(23.3, 46.75);
-    final bigNest = BigDashNestBumper(id: 'big_nest')
-      ..initialPosition = Vector2(18.55, 59.35);
-
-    await addAll([
-      signPost,
-      smallLeftNest,
-      smallRightNest,
-      bigNest,
-    ]);
-  }
 }
 
-/// {@template dash_nest_bumper}
-/// Bumper located in the [FlutterForest].
-/// {@endtemplate}
-@visibleForTesting
-abstract class DashNestBumper extends BodyComponent<PinballGame>
-    with ScorePoints, InitialPosition {
-  /// {@macro dash_nest_bumper}
-  DashNestBumper({required this.id}) {
-    paint = Paint()
-      ..color = Colors.blue.withOpacity(0.5)
-      ..style = PaintingStyle.fill;
+class _ControlledBigDashNestBumper extends BigDashNestBumper
+    with Controls<DashNestBumperController>, ScorePoints {
+  _ControlledBigDashNestBumper({required String id}) : super() {
+    controller = DashNestBumperController(this, id: id);
   }
-
-  /// Unique identifier for this [DashNestBumper].
-  ///
-  /// Used to identify [DashNestBumper]s in [GameState.activatedDashNests].
-  final String id;
-}
-
-/// Listens when a [Ball] bounces bounces against a [DashNestBumper].
-@visibleForTesting
-class DashNestBumperBallContactCallback
-    extends ContactCallback<DashNestBumper, Ball> {
-  @override
-  void begin(DashNestBumper dashNestBumper, Ball ball, Contact _) {
-    dashNestBumper.gameRef.read<GameBloc>().add(
-          DashNestActivated(dashNestBumper.id),
-        );
-  }
-}
-
-/// {@macro dash_nest_bumper}
-@visibleForTesting
-class BigDashNestBumper extends DashNestBumper {
-  /// {@macro dash_nest_bumper}
-  BigDashNestBumper({required String id}) : super(id: id);
 
   @override
   int get points => 20;
-
-  @override
-  Body createBody() {
-    final shape = EllipseShape(
-      center: Vector2.zero(),
-      majorRadius: 4.85,
-      minorRadius: 3.95,
-    )..rotate(math.pi / 2);
-    final fixtureDef = FixtureDef(shape);
-
-    final bodyDef = BodyDef()
-      ..position = initialPosition
-      ..userData = this;
-
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
-  }
 }
 
-/// {@macro dash_nest_bumper}
-@visibleForTesting
-class SmallDashNestBumper extends DashNestBumper {
-  /// {@macro dash_nest_bumper}
-  SmallDashNestBumper({required String id}) : super(id: id);
+class _ControlledSmallDashNestBumper extends SmallDashNestBumper
+    with Controls<DashNestBumperController>, ScorePoints {
+  _ControlledSmallDashNestBumper.a({required String id}) : super.a() {
+    controller = DashNestBumperController(this, id: id);
+  }
+
+  _ControlledSmallDashNestBumper.b({required String id}) : super.b() {
+    controller = DashNestBumperController(this, id: id);
+  }
 
   @override
   int get points => 10;
+}
+
+/// {@template dash_nest_bumper_controller}
+/// Controls a [DashNestBumper].
+/// {@endtemplate}
+@visibleForTesting
+class DashNestBumperController extends ComponentController<DashNestBumper>
+    with BlocComponent<GameBloc, GameState>, HasGameRef<PinballGame> {
+  /// {@macro dash_nest_bumper_controller}
+  DashNestBumperController(
+    DashNestBumper dashNestBumper, {
+    required this.id,
+  }) : super(dashNestBumper);
+
+  /// Unique identifier for the controlled [DashNestBumper].
+  ///
+  /// Used to identify [DashNestBumper]s in [GameState.activatedDashNests].
+  final String id;
 
   @override
-  Body createBody() {
-    final shape = EllipseShape(
-      center: Vector2.zero(),
-      majorRadius: 3,
-      minorRadius: 2.25,
-    )..rotate(math.pi / 2);
-    final fixtureDef = FixtureDef(shape)
-      ..friction = 0
-      ..restitution = 4;
+  bool listenWhen(GameState? previousState, GameState newState) {
+    final wasActive = previousState?.activatedDashNests.contains(id) ?? false;
+    final isActive = newState.activatedDashNests.contains(id);
 
-    final bodyDef = BodyDef()
-      ..position = initialPosition
-      ..userData = this;
+    return wasActive != isActive;
+  }
 
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  @override
+  void onNewState(GameState state) {
+    super.onNewState(state);
+
+    if (state.activatedDashNests.contains(id)) {
+      component.activate();
+    } else {
+      component.deactivate();
+    }
+  }
+
+  /// Registers when a [DashNestBumper] is hit by a [Ball].
+  ///
+  /// Triggered by [_ControlledDashNestBumperBallContactCallback].
+  void hit() {
+    gameRef.read<GameBloc>().add(DashNestActivated(id));
+  }
+}
+
+/// Listens when a [Ball] bounces bounces against a [DashNestBumper].
+class _ControlledDashNestBumperBallContactCallback
+    extends ContactCallback<Controls<DashNestBumperController>, Ball> {
+  @override
+  void begin(
+    Controls<DashNestBumperController> controlledDashNestBumper,
+    Ball _,
+    Contact __,
+  ) {
+    controlledDashNestBumper.controller.hit();
   }
 }
