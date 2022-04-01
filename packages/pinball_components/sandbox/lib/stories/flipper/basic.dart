0927a4ab97948b89bf43c0c503b49ce347ab146f
alestiago <dev@alestiago.com>
2022-04-01 11:39:58 +0100

    feat: made Flippers movable

--- packages/pinball_components/sandbox/lib/stories/flipper/basic.dart
+++ packages/pinball_components/sandbox/lib/stories/flipper/basic.dart
@@ -1,11 +1,25 @@
-import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pinball_components/pinball_components.dart';
 import 'package:sandbox/common/common.dart';
 
-class BasicFlipperGame extends BasicGame {
+class BasicFlipperGame extends BasicGame with KeyboardEvents {
   static const info = '''
       Basic example of how a Flipper works.
 ''';
+
+  static final _leftFlipperKeys = {
+    LogicalKeyboardKey.arrowLeft,
+    LogicalKeyboardKey.keyA,
+  };
+
+  static final _rightFlipperKeys = {
+    LogicalKeyboardKey.arrowRight,
+  };
+
+  late Flipper leftFlipper;
+  late Flipper rightFlipper;
 
   @override
   Future<void> onLoad() async {
@@ -13,14 +27,40 @@ class BasicFlipperGame extends BasicGame {
 
     final center = screenToWorld(camera.viewport.canvasSize! / 2);
 
-    final leftFlipper = Flipper(side: BoardSide.left)
+    leftFlipper = Flipper(side: BoardSide.left)
       ..initialPosition = center - Vector2(Flipper.size.x, 0);
-    final rightFlipper = Flipper(side: BoardSide.right)
+    rightFlipper = Flipper(side: BoardSide.right)
       ..initialPosition = center + Vector2(Flipper.size.x, 0);
 
     await addAll([
       leftFlipper,
       rightFlipper,
     ]);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final movedLeftFlipper =
+        _leftFlipperKeys.intersection(keysPressed).isNotEmpty;
+    if (event is RawKeyDownEvent && movedLeftFlipper) {
+      leftFlipper.moveUp();
+    } else if (event is RawKeyUpEvent && movedLeftFlipper) {
+      leftFlipper.moveDown();
+    }
+
+    final movedRightFlipper =
+        _rightFlipperKeys.intersection(keysPressed).isNotEmpty;
+    if (event is RawKeyDownEvent && movedRightFlipper) {
+      rightFlipper.moveUp();
+    } else if (event is RawKeyUpEvent && movedRightFlipper) {
+      rightFlipper.moveDown();
+    }
+
+    return movedLeftFlipper || movedRightFlipper
+        ? KeyEventResult.handled
+        : KeyEventResult.ignored;
   }
 }
