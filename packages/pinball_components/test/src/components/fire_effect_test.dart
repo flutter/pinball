fd217ffcbca321ef63211689ad9ab06df61d9abb
alestiago <dev@alestiago.com>
2022-04-01 12:32:15 +0100

    refactor: simplified tests

--- packages/pinball_components/test/src/components/fire_effect_test.dart
+++ packages/pinball_components/test/src/components/fire_effect_test.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: cascade_invocations
 
-import 'dart:ui';
-
 import 'package:flame/components.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:pinball_components/pinball_components.dart';
 
 import '../../helpers/helpers.dart';
@@ -14,26 +11,16 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   final flameTester = FlameTester(TestGame.new);
 
-  setUpAll(() {
-    registerFallbackValue(Offset.zero);
-    registerFallbackValue(Paint());
-  });
-
-  group('FireEffect', () {
-    flameTester.test('is removed once its particles are done', (game) async {
-      await game.ensureAdd(
-        FireEffect(
-          burstPower: 1,
-          position: Vector2.zero(),
-          direction: Vector2.all(2),
-        ),
+  flameTester.test(
+    'loads correctly',
+    (game) async {
+      final fireEffect = FireEffect(
+        burstPower: 1,
+        direction: Vector2.zero(),
       );
-      await game.ready();
-      expect(game.children.whereType<FireEffect>().length, equals(1));
-      game.update(5);
+      await game.ensureAdd(fireEffect);
 
-      await game.ready();
-      expect(game.children.whereType<FireEffect>().length, equals(0));
-    });
-  });
+      expect(game.contains(fireEffect), isTrue);
+    },
+  );
 }
