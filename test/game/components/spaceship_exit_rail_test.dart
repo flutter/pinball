2fcd465eaad347ceae947172bc6fa3dc66fa4a6d
Rui Miguel Alonso <ruiskas@gmail.com>
2022-04-01 14:13:23 +0200

    Update test/game/components/spaceship_exit_rail_test.dart
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

--- test/game/components/spaceship_exit_rail_test.dart
+++ test/game/components/spaceship_exit_rail_test.dart
@@ -31,6 +31,8 @@ void main() {
       when(() => fixture.filterData).thenReturn(filterData);
     });
 
+   // TODO(alestiago): Make ContactCallback private and use `beginContact`
+   // instead.
     group('SpaceshipExitHoleBallContactCallback', () {
       setUp(() {
         when(() => ball.priority).thenReturn(1);
