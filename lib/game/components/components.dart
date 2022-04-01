b243c063a3c990d208de20bb9a45025629bf5dc6
Alejandro Santiago <dev@alestiago.com>
2022-04-01 21:04:05 +0100

    refactor: moved Kicker to pinball_components (#133)

--- lib/game/components/components.dart
+++ lib/game/components/components.dart
@@ -5,7 +5,6 @@ export 'controlled_ball.dart';
 export 'controlled_flipper.dart';
 export 'flutter_forest.dart';
 export 'jetpack_ramp.dart';
-export 'kicker.dart';
 export 'launcher_ramp.dart';
 export 'plunger.dart';
 export 'score_points.dart';
