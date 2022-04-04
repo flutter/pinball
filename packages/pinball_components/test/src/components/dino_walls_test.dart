455f65e318270d6db7308fb9c40b950393cd1974
Allison Ryan <allisonryan0002@gmail.com>
2022-04-04 13:05:52 -0500

    test: remove position

--- packages/pinball_components/test/src/components/dino_walls_test.dart
+++ packages/pinball_components/test/src/components/dino_walls_test.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: cascade_invocations
 
-import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pinball_components/pinball_components.dart';
@@ -15,7 +14,7 @@ void main() {
     flameTester.test(
       'loads correctly',
       (game) async {
-        final dinoWalls = DinoWalls(position: Vector2.zero());
+        final dinoWalls = DinoWalls();
         await game.addFromBlueprint(dinoWalls);
         await game.ready();
 
