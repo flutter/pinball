a8e7c9bf85a442a6859c2c4202a991332525d55a
alestiago <dev@alestiago.com>
2022-04-07 13:33:31 +0100

    refactor: removed unused import

--- packages/pinball_components/sandbox/lib/stories/flipper/basic_flipper_game.dart
+++ packages/pinball_components/sandbox/lib/stories/flipper/basic_flipper_game.dart
@@ -1,5 +1,4 @@
 import 'package:flame/input.dart';
-import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pinball_components/pinball_components.dart';
