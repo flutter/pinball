b76f646166b025a6efc405a4f9b8251a8b2b90fd
Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>
2022-04-07 10:51:17 -0500

    chore: trace all bodies in sandbox (#157)
    * feat: add traceAllBodies method
    
    * chore: update tracing stories
    
    * refactor: use for each
    
    * refactor: convert to mixin
    
    * refactor: simplify mixin

--- packages/pinball_components/sandbox/lib/stories/dash_nest_bumper/big_dash_nest_bumper_game.dart
+++ packages/pinball_components/sandbox/lib/stories/dash_nest_bumper/big_dash_nest_bumper_game.dart
@@ -5,18 +5,14 @@ import 'package:pinball_components/pinball_components.dart';
 import 'package:sandbox/common/common.dart';
 import 'package:sandbox/stories/ball/basic_ball_game.dart';
 
-class BigDashNestBumperGame extends BasicBallGame {
-  BigDashNestBumperGame({
-    required this.trace,
-  }) : super(color: const Color(0xFF0000FF));
+class BigDashNestBumperGame extends BasicBallGame with Traceable {
+  BigDashNestBumperGame() : super(color: const Color(0xFF0000FF));
 
   static const info = '''
     Shows how a BigDashNestBumper is rendered.
 
     Activate the "trace" parameter to overlay the body.
 ''';
-
-  final bool trace;
 
   @override
   Future<void> onLoad() async {
@@ -28,6 +24,6 @@ class BigDashNestBumperGame extends BasicBallGame {
       ..priority = 1;
     await add(bigDashNestBumper);
 
-    if (trace) bigDashNestBumper.trace();
+    await traceAllBodies();
   }
 }
