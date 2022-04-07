d86ef40cb9c051414e60c78ef9150ee6209c93e9
Alejandro Santiago <dev@alestiago.com>
2022-04-07 14:30:56 +0100

    feat: Flipper joint adjustment (#159)
    * feat(sandbox): removed FlipperTracingGame
    
    * feat: adjusted flipper anchor
    
    * refacto: adjusted anchoring points
    
    * refactor: removed unused import
    
    * refactor: renamed FlipperGame

--- packages/pinball_components/sandbox/lib/common/trace.dart
+++ packages/pinball_components/sandbox/lib/common/trace.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:pinball_components/pinball_components.dart';
 
 extension BodyTrace on BodyComponent {
   void trace({Color color = const Color(0xFFFF0000)}) {
@@ -13,6 +14,12 @@ extension BodyTrace on BodyComponent {
       mounted.whenComplete(() {
         final sprite = children.whereType<SpriteComponent>().first;
         sprite.paint.color = sprite.paint.color.withOpacity(0.5);
+
+        descendants().whereType<JointAnchor>().forEach((anchor) {
+          final fixtureDef = FixtureDef(CircleShape()..radius = 0.5);
+          anchor.body.createFixture(fixtureDef);
+          anchor.renderBody = true;
+        });
       }),
     );
   }
