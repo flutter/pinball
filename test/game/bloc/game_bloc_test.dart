63973b1062cb06f7e3df158a7faaa42a3f257cbc
Rui Miguel Alonso <ruiskas@gmail.com>
2022-04-07 17:20:22 +0200

    Update test/game/bloc/game_bloc_test.dart
    Co-authored-by: Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>

--- test/game/bloc/game_bloc_test.dart
+++ test/game/bloc/game_bloc_test.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pinball/game/game.dart';
