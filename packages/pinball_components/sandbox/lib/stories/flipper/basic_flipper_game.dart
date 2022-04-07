1ae00bebf0b5fe029457d9ce6416a0dcf0fc0133
alestiago <dev@alestiago.com>
2022-04-07 13:22:56 +0100

    feat: adjusted flipper anchor

--- packages/pinball_components/sandbox/lib/stories/flipper/basic_flipper_game.dart
+++ packages/pinball_components/sandbox/lib/stories/flipper/basic_flipper_game.dart
@@ -1,4 +1,5 @@
 import 'package:flame/input.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pinball_components/pinball_components.dart';
