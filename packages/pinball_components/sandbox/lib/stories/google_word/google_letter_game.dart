59de86c69e5159658993a4e0c53700ed96f0af68
alestiago <dev@alestiago.com>
2022-04-11 12:03:52 +0100

    refactor: updated constructor

--- packages/pinball_components/sandbox/lib/stories/google_word/google_letter_game.dart
+++ packages/pinball_components/sandbox/lib/stories/google_word/google_letter_game.dart
@@ -20,7 +20,7 @@ class GoogleLetterGame extends BasicBallGame {
     addContactCallback(_BallGoogleLetterContactCallback());
 
     camera.followVector2(Vector2.zero());
-    await add(GoogleLetter());
+    await add(GoogleLetter.letter1());
 
     if (trace) await traceAllBodies();
   }
