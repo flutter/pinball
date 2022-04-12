ecc3268bd55a30192fd6b9112a3806db3b010181
arturplaczek <arturp1989@gmail.com>
2022-04-12 16:55:59 +0200

    chore: add png assets

--- packages/pinball_theme/lib/src/generated/assets.gen.dart
+++ packages/pinball_theme/lib/src/generated/assets.gen.dart
@@ -3,15 +3,80 @@
 ///  FlutterGen
 /// *****************************************************
 
+// ignore_for_file: directives_ordering,unnecessary_import
+
 import 'package:flutter/widgets.dart';
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
+  /// File path: assets/images/android.png
   AssetGenImage get android => const AssetGenImage('assets/images/android.png');
+
+  /// File path: assets/images/android_background.png
+  AssetGenImage get androidBackground =>
+      const AssetGenImage('assets/images/android_background.png');
+
+  /// File path: assets/images/android_icon.png
+  AssetGenImage get androidIcon =>
+      const AssetGenImage('assets/images/android_icon.png');
+
+  /// File path: assets/images/android_placeholder.png
+  AssetGenImage get androidPlaceholder =>
+      const AssetGenImage('assets/images/android_placeholder.png');
+
+  /// File path: assets/images/dash.png
   AssetGenImage get dash => const AssetGenImage('assets/images/dash.png');
+
+  /// File path: assets/images/dash_background.png
+  AssetGenImage get dashBackground =>
+      const AssetGenImage('assets/images/dash_background.png');
+
+  /// File path: assets/images/dash_icon.png
+  AssetGenImage get dashIcon =>
+      const AssetGenImage('assets/images/dash_icon.png');
+
+  /// File path: assets/images/dash_placeholder.png
+  AssetGenImage get dashPlaceholder =>
+      const AssetGenImage('assets/images/dash_placeholder.png');
+
+  /// File path: assets/images/dino.png
   AssetGenImage get dino => const AssetGenImage('assets/images/dino.png');
+
+  /// File path: assets/images/dino_background.png
+  AssetGenImage get dinoBackground =>
+      const AssetGenImage('assets/images/dino_background.png');
+
+  /// File path: assets/images/dino_icon.png
+  AssetGenImage get dinoIcon =>
+      const AssetGenImage('assets/images/dino_icon.png');
+
+  /// File path: assets/images/dino_placeholder.png
+  AssetGenImage get dinoPlaceholder =>
+      const AssetGenImage('assets/images/dino_placeholder.png');
+
+  /// File path: assets/images/pinball_button.png
+  AssetGenImage get pinballButton =>
+      const AssetGenImage('assets/images/pinball_button.png');
+
+  /// File path: assets/images/select_character_background.png
+  AssetGenImage get selectCharacterBackground =>
+      const AssetGenImage('assets/images/select_character_background.png');
+
+  /// File path: assets/images/sparky.png
   AssetGenImage get sparky => const AssetGenImage('assets/images/sparky.png');
+
+  /// File path: assets/images/sparky_background.png
+  AssetGenImage get sparkyBackground =>
+      const AssetGenImage('assets/images/sparky_background.png');
+
+  /// File path: assets/images/sparky_icon.png
+  AssetGenImage get sparkyIcon =>
+      const AssetGenImage('assets/images/sparky_icon.png');
+
+  /// File path: assets/images/sparky_placeholder.png
+  AssetGenImage get sparkyPlaceholder =>
+      const AssetGenImage('assets/images/sparky_placeholder.png');
 }
 
 class Assets {
