0a5a410ec70d8424a297fe90908be0fc515a1ab6
RuiAlonso <rui.alonso@verygood.ventures>
2022-04-13 20:08:51 +0200

    refactor: changed plunger position

--- lib/game/components/launcher.dart
+++ lib/game/components/launcher.dart
@@ -15,8 +15,8 @@ class Launcher extends Forge2DBlueprint {
 
   @override
   void build(Forge2DGame gameRef) {
-    plunger = ControlledPlunger(compressionDistance: 12.3)
-      ..initialPosition = Vector2(40.1, -38);
+    plunger = ControlledPlunger(compressionDistance: 20) //12.3
+      ..initialPosition = Vector2(38.5, -20);
 
     final _rocket = RocketSpriteComponent()..position = Vector2(43, 62);
 
