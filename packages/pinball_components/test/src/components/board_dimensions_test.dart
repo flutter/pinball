9714bead895f3b486a62938a79ddc050d00b7d85
Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>
2022-04-14 12:27:54 -0500

    feat: gravity override (#192)

--- packages/pinball_components/test/src/components/board_dimensions_test.dart
+++ packages/pinball_components/test/src/components/board_dimensions_test.dart
@@ -19,9 +19,5 @@ void main() {
     test('has perspectiveShrinkFactor', () {
       expect(BoardDimensions.perspectiveShrinkFactor, equals(0.63));
     });
-
-    test('has shrinkAdjustedHeight', () {
-      expect(BoardDimensions.shrinkAdjustedHeight, isNotNull);
-    });
   });
 }
