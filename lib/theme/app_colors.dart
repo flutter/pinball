ae911723781f6d5002fd9cb8f2bde046ba4595a7
arturplaczek <arturp1989@gmail.com>
2022-04-15 10:37:29 +0200

    fix: update colors & fonts

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -5,27 +5,18 @@ import 'package:flutter/material.dart';
 
 /// Defines the color palette for the App UI Kit.
 abstract class AppColors {
-  /// Black
-  static const Color black = Color(0xFF000000);
-
-  /// White
+  /// White color.
   static const Color white = Color(0xFFFFFFFF);
 
-  /// Transparent
-  static const Color transparent = Color(0x00000000);
-
-  /// Blue
-  static const Color blue = Color(0xFF4B94F6);
-
-  /// Dark blue
+  /// Dark blue color.
   static const Color darkBlue = Color(0xFF0C32A4);
 
-  /// Orange
+  /// Orange color.
   static const Color orange = Color(0xFFFFEE02);
 
-  /// Blue
-  static const Color appBarColor = Color(0xFF13B9FF);
+  /// Blue color.
+  static const Color blue = Color(0xFF4B94F6);
 
-  /// Pinball background color.
-  static const Color background = blue;
+  /// Transparent color.
+  static const Color transparent = Color(0x00000000);
 }
--- lib/theme/app_text_style.dart
+++ lib/theme/app_text_style.dart
@@ -1,104 +1,38 @@
 import 'package:flutter/widgets.dart';
+import 'package:pinball/theme/theme.dart';
 import 'package:pinball_components/gen/fonts.gen.dart';
+
+const _fontPackage = 'pinball_components';
+const _primaryFontFamily = FontFamily.pixeloidSans;
 
 /// App Text Style Definitions
 abstract class AppTextStyle {
-  /// Headline 1 Text Style
-  static const headline1 = TextStyle(
-    fontSize: 36,
-    height: 1.25,
-    package: 'pinball_components',
-    fontFamily: FontFamily.pixeloidSans,
-  );
-
-  /// Headline 2 Text Style
+  /// Headline 2 Text Style with font size 28.
   static const headline2 = TextStyle(
     fontSize: 28,
-    height: 1.25,
-    package: 'pinball_components',
-    fontFamily: FontFamily.pixeloidSans,
+    package: _fontPackage,
+    fontFamily: _primaryFontFamily,
   );
 
-  /// Headline 3 Text Style
+  /// Headline 3 Text Style with font size 24.
   static const headline3 = TextStyle(
     fontSize: 24,
-    height: 1.25,
-    package: 'pinball_components',
-    fontFamily: FontFamily.pixeloidSans,
+    package: _fontPackage,
+    fontFamily: _primaryFontFamily,
   );
 
-  /// Headline 4 Text Style
+  /// Headline 4 Text Style with font size 20 and white color.
   static const headline4 = TextStyle(
+    color: AppColors.white,
     fontSize: 20,
-    fontFamily: FontFamily.pixeloidSans,
-    letterSpacing: 3,
-    package: 'pinball_components',
+    package: _fontPackage,
+    fontFamily: _primaryFontFamily,
   );
 
-  /// Headline 5 Text Style
-  static const headline5 = TextStyle(
-    fontSize: 18,
-    fontFamily: FontFamily.pixeloidSans,
-    height: 1.25,
-    package: 'pinball_components',
-  );
-
-  /// Headline 6 Text Style
-  static const headline6 = TextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.pixeloidSans,
-    height: 1.25,
-    package: 'pinball_components',
-  );
-
-  /// Subtitle 1 Text Style
+  /// Subtitle 1 Text Style with font size 10.
   static const subtitle1 = TextStyle(
     fontSize: 10,
-    fontFamily: FontFamily.pixeloidSans,
-    package: 'pinball_components',
-  );
-
-  /// Subtitle 2 Text Style
-  static const subtitle2 = TextStyle(
-    fontSize: 14,
-    fontFamily: FontFamily.pixeloidSans,
-    package: 'pinball_components',
-  );
-
-  /// Body Text 1 Text Style
-  static const bodyText1 = TextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.pixeloidSans,
-    package: 'pinball_components',
-  );
-
-  /// Body Text 2 Text Style (the default)
-  static const bodyText2 = TextStyle(
-    fontSize: 14,
-    fontFamily: FontFamily.pixeloidSans,
-    package: 'pinball_components',
-  );
-
-  /// Button Text Style
-  static const button = TextStyle(
-    fontSize: 18,
-    fontFamily: FontFamily.pixeloidSans,
-    package: 'pinball_components',
-  );
-
-  /// Caption Text Style
-  static const caption = TextStyle(
-    fontSize: 12,
-    fontFamily: FontFamily.pixeloidSans,
-    package: 'pinball_components',
-  );
-
-  /// Overline Text Style
-  static const overline = TextStyle(
-    fontSize: 12,
-    letterSpacing: 1.15,
-    package: 'pinball_components',
-    fontFamily: FontFamily.pixeloidSans,
-    decoration: TextDecoration.underline,
+    fontFamily: _primaryFontFamily,
+    package: _fontPackage,
   );
 }
