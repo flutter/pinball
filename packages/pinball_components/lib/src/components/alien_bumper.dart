f5c3c5fb56033e2bd013a4add349caf51e0edb0c
Allison Ryan <allisonryan0002@gmail.com>
2022-04-19 16:42:44 -0500

    refactor: bumpers to use group components

--- packages/pinball_components/lib/src/components/alien_bumper.dart
+++ packages/pinball_components/lib/src/components/alien_bumper.dart
@@ -1,36 +1,40 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/material.dart';
 import 'package:pinball_components/pinball_components.dart';
 
 /// {@template alien_bumper}
-/// Bumper for Alien area.
+/// Bumper for area under the [Spaceship].
 /// {@endtemplate}
-// TODO(ruimiguel): refactor later to unify with DashBumpers.
 class AlienBumper extends BodyComponent with InitialPosition {
   /// {@macro alien_bumper}
   AlienBumper._({
     required double majorRadius,
     required double minorRadius,
-    required String activeAssetPath,
-    required String inactiveAssetPath,
-    required SpriteComponent spriteComponent,
+    required String onAssetPath,
+    required String offAssetPath,
   })  : _majorRadius = majorRadius,
         _minorRadius = minorRadius,
-        _activeAssetPath = activeAssetPath,
-        _inactiveAssetPath = inactiveAssetPath,
-        _spriteComponent = spriteComponent;
+        super(
+          children: [
+            _AlienBumperSpriteGroupComponent(
+              onAssetPath: onAssetPath,
+              offAssetPath: offAssetPath,
+            ),
+          ],
+        ) {
+    renderBody = false;
+  }
 
   /// {@macro alien_bumper}
   AlienBumper.a()
       : this._(
           majorRadius: 3.52,
           minorRadius: 2.97,
-          activeAssetPath: Assets.images.alienBumper.a.active.keyName,
-          inactiveAssetPath: Assets.images.alienBumper.a.inactive.keyName,
-          spriteComponent: SpriteComponent(
-            anchor: Anchor.center,
-            position: Vector2(0, -0.1),
-          ),
+          onAssetPath: Assets.images.alienBumper.a.on.keyName,
+          offAssetPath: Assets.images.alienBumper.a.off.keyName,
         );
 
   /// {@macro alien_bumper}
@@ -38,32 +42,12 @@ class AlienBumper extends BodyComponent with InitialPosition {
       : this._(
           majorRadius: 3.19,
           minorRadius: 2.79,
-          activeAssetPath: Assets.images.alienBumper.b.active.keyName,
-          inactiveAssetPath: Assets.images.alienBumper.b.inactive.keyName,
-          spriteComponent: SpriteComponent(
-            anchor: Anchor.center,
-            position: Vector2(0, -0.1),
-          ),
+          onAssetPath: Assets.images.alienBumper.b.on.keyName,
+          offAssetPath: Assets.images.alienBumper.a.off.keyName,
         );
 
   final double _majorRadius;
   final double _minorRadius;
-  final String _activeAssetPath;
-  late final Sprite _activeSprite;
-  final String _inactiveAssetPath;
-  late final Sprite _inactiveSprite;
-  final SpriteComponent _spriteComponent;
-
-  @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-    renderBody = false;
-
-    await _loadSprites();
-
-    deactivate();
-    await add(_spriteComponent);
-  }
 
   @override
   Body createBody() {
@@ -84,25 +68,54 @@ class AlienBumper extends BodyComponent with InitialPosition {
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 
-  Future<void> _loadSprites() async {
-    // TODO(alestiago): I think ideally we would like to do:
-    // Sprite(path).load so we don't require to store the activeAssetPath and
-    // the inactive assetPath.
-    _inactiveSprite = await gameRef.loadSprite(_inactiveAssetPath);
-    _activeSprite = await gameRef.loadSprite(_activeAssetPath);
+  /// Animates the [AlienBumper].
+  Future<void> animate() async {
+    final spriteGroupComponent = firstChild<_AlienBumperSpriteGroupComponent>()
+      ?..current = AlienBumperSpriteState.off;
+    await Future<void>.delayed(const Duration(milliseconds: 50));
+    spriteGroupComponent?.current = AlienBumperSpriteState.on;
   }
+}
 
-  /// Activates the [AlienBumper].
-  void activate() {
-    _spriteComponent
-      ..sprite = _activeSprite
-      ..size = _activeSprite.originalSize / 10;
-  }
+@visibleForTesting
 
-  /// Deactivates the [AlienBumper].
-  void deactivate() {
-    _spriteComponent
-      ..sprite = _inactiveSprite
-      ..size = _inactiveSprite.originalSize / 10;
+/// Indicates the [AlienBumper]'s current sprite state.
+enum AlienBumperSpriteState {
+  /// The on sprite is being displayed.
+  on,
+
+  /// The off sprite is being displayed.
+  off,
+}
+
+class _AlienBumperSpriteGroupComponent
+    extends SpriteGroupComponent<AlienBumperSpriteState> with HasGameRef {
+  _AlienBumperSpriteGroupComponent({
+    required String onAssetPath,
+    required String offAssetPath,
+  })  : _onAssetPath = onAssetPath,
+        _offAssetPath = offAssetPath,
+        super(
+          anchor: Anchor.center,
+          position: Vector2(0, -0.1),
+        );
+
+  final String _onAssetPath;
+  final String _offAssetPath;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    final sprites = {
+      AlienBumperSpriteState.on: Sprite(gameRef.images.fromCache(_onAssetPath)),
+      AlienBumperSpriteState.off:
+          Sprite(gameRef.images.fromCache(_offAssetPath)),
+    };
+
+    this.sprites = sprites;
+    size = sprites[AlienBumperSpriteState.on]!.originalSize / 10;
+
+    current = AlienBumperSpriteState.on;
   }
 }
