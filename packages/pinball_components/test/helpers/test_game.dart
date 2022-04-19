aa86ef302b24efbaab4155a75b0b38cfcf39d839
Allison Ryan <allisonryan0002@gmail.com>
2022-04-19 16:44:19 -0500

    refactor: load assets in test games

--- packages/pinball_components/test/helpers/test_game.dart
+++ packages/pinball_components/test/helpers/test_game.dart
@@ -2,8 +2,18 @@ import 'package:flame/input.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
 class TestGame extends Forge2DGame {
-  TestGame() {
+  TestGame([List<String>? assets]) : _assets = assets {
     images.prefix = '';
+  }
+
+  final List<String>? _assets;
+
+  @override
+  Future<void> onLoad() async {
+    if (_assets != null) {
+      await Future.wait(_assets!.map(images.load));
+    }
+    await super.onLoad();
   }
 }
 
