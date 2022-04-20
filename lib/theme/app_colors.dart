3262c5f8a0c605eb638deaa02ea95c5b06f9ae84
arturplaczek <arturp1989@gmail.com>
2022-04-20 15:07:33 +0200

    fix: remove comments

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,19 +1,15 @@
+// ignore_for_file: public_member_api_docs
+
 import 'package:flutter/material.dart';
 
-/// Defines the color palette for the App UI Kit.
 abstract class AppColors {
-  /// White color.
   static const Color white = Color(0xFFFFFFFF);
 
-  /// Dark blue color.
   static const Color darkBlue = Color(0xFF0C32A4);
 
-  /// Orange color.
   static const Color orange = Color(0xFFFFEE02);
 
-  /// Blue color.
   static const Color blue = Color(0xFF4B94F6);
 
-  /// Transparent color.
   static const Color transparent = Color(0x00000000);
 }
--- lib/theme/app_text_style.dart
+++ lib/theme/app_text_style.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs
+
 import 'package:flutter/widgets.dart';
 import 'package:pinball/theme/theme.dart';
 import 'package:pinball_components/pinball_components.dart';
@@ -5,31 +7,26 @@ import 'package:pinball_components/pinball_components.dart';
 const _fontPackage = 'pinball_components';
 const _primaryFontFamily = FontFamily.pixeloidSans;
 
-/// App Text Style Definitions
 abstract class AppTextStyle {
-  /// Headline 2 Text Style with font size 28.
-  static const headline2 = TextStyle(
+  static const headline1 = TextStyle(
     fontSize: 28,
     package: _fontPackage,
     fontFamily: _primaryFontFamily,
   );
 
-  /// Headline 3 Text Style with font size 24.
-  static const headline3 = TextStyle(
+  static const headline2 = TextStyle(
     fontSize: 24,
     package: _fontPackage,
     fontFamily: _primaryFontFamily,
   );
 
-  /// Headline 4 Text Style with font size 20 and white color.
-  static const headline4 = TextStyle(
+  static const headline3 = TextStyle(
     color: AppColors.white,
     fontSize: 20,
     package: _fontPackage,
     fontFamily: _primaryFontFamily,
   );
 
-  /// Subtitle 1 Text Style with font size 10.
   static const subtitle1 = TextStyle(
     fontSize: 10,
     fontFamily: _primaryFontFamily,
