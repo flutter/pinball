4c28a7eb2961ca5b392bf4d8f69915293926ae18
Allison Ryan <allisonryan0002@gmail.com>
2022-04-20 14:47:05 -0500

    refactor: signpost tests from cache

--- test/game/components/board_test.dart
+++ test/game/components/board_test.dart
@@ -16,6 +16,10 @@ void main() {
     Assets.images.dash.bumper.a.inactive.keyName,
     Assets.images.dash.bumper.b.active.keyName,
     Assets.images.dash.bumper.b.inactive.keyName,
+    Assets.images.signpost.inactive.keyName,
+    Assets.images.signpost.active1.keyName,
+    Assets.images.signpost.active2.keyName,
+    Assets.images.signpost.active3.keyName,
   ];
   final flameTester = FlameTester(() => EmptyPinballTestGame(assets));
 
