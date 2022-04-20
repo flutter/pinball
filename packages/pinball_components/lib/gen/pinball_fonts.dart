51ddc42236361b603bed5d7e626e4ac07b2b59ef
arturplaczek <arturp1989@gmail.com>
2022-04-20 15:30:27 +0200

    fix: make PinballFonts const

--- packages/pinball_components/lib/gen/pinball_fonts.dart
+++ packages/pinball_components/lib/gen/pinball_fonts.dart
@@ -1,16 +1,14 @@
 import 'package:pinball_components/gen/fonts.gen.dart';
 
-String _prefixFont(String font) {
-  return 'packages/pinball_components/$font';
-}
-
 /// Class with the fonts available on the pinball game
 class PinballFonts {
   PinballFonts._();
 
   /// Mono variation of the Pixeloid font
-  static final String pixeloidMono = _prefixFont(FontFamily.pixeloidMono);
+  static const String pixeloidMono =
+      'packages/pinball_components/${FontFamily.pixeloidMono}';
 
   /// Sans variation of the Pixeloid font
-  static final String pixeloidSans = _prefixFont(FontFamily.pixeloidMono);
+  static const String pixeloidSans =
+      'packages/pinball_components/${FontFamily.pixeloidMono}';
 }
