d1a6c56c2cbb545b258b9f33687332b9da5ca64e
arturplaczek <arturp1989@gmail.com>
2022-04-20 17:32:59 +0200

    chore: update  PinballFonts

--- packages/pinball_components/lib/gen/pinball_fonts.dart
+++ packages/pinball_components/lib/gen/pinball_fonts.dart
@@ -1,14 +1,14 @@
 import 'package:pinball_components/gen/fonts.gen.dart';
 
+const String _fontPath = 'packages/pinball_components/';
+
 /// Class with the fonts available on the pinball game
 class PinballFonts {
   PinballFonts._();
 
   /// Mono variation of the Pixeloid font
-  static const String pixeloidMono =
-      'packages/pinball_components/${FontFamily.pixeloidMono}';
+  static const String pixeloidMono = '$_fontPath/${FontFamily.pixeloidMono}';
 
   /// Sans variation of the Pixeloid font
-  static const String pixeloidSans =
-      'packages/pinball_components/${FontFamily.pixeloidMono}';
+  static const String pixeloidSans = '$_fontPath/${FontFamily.pixeloidSans}';
 }
