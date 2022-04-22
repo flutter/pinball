ce9a677c466f0d8991a19ab2903705768335f9d0
Allison Ryan <allisonryan0002@gmail.com>
2022-04-21 22:21:03 -0500

    fix: sanbox stories

--- packages/pinball_components/sandbox/lib/stories/baseboard/stories.dart
+++ packages/pinball_components/sandbox/lib/stories/baseboard/stories.dart
@@ -1,15 +1,15 @@
 import 'package:dashbook/dashbook.dart';
 import 'package:flame/game.dart';
 import 'package:sandbox/common/common.dart';
-import 'package:sandbox/stories/baseboard/basic_baseboard_game.dart';
+import 'package:sandbox/stories/baseboard/baseboard_game.dart';
 
 void addBaseboardStories(Dashbook dashbook) {
   dashbook.storiesOf('Baseboard').add(
         'Basic',
         (context) => GameWidget(
-          game: BasicBaseboardGame(),
+          game: BaseboardGame()..trace = context.boolProperty('Trace', true),
         ),
-        codeLink: buildSourceLink('baseboard/basic.dart'),
-        info: BasicBaseboardGame.info,
+        codeLink: buildSourceLink('baseboard_game/basic.dart'),
+        info: BaseboardGame.info,
       );
 }
