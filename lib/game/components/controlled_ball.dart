4f30e599149bcc62bc7e0a8bea7790c9a9479009
RuiAlonso <rui.alonso@verygood.ventures>
2022-04-26 12:32:27 +0200

    refactor: BallAdded event on ball mounted

--- lib/game/components/controlled_ball.dart
+++ lib/game/components/controlled_ball.dart
@@ -1,4 +1,6 @@
 import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame_bloc/flame_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:pinball/game/game.dart';
 import 'package:pinball_components/pinball_components.dart';
