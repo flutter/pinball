72a377b1b0bbb4185f1725093cebcf50749a482b
alestiago <dev@alestiago.com>
2022-04-27 10:21:53 +0100

    test: included ContactBehavior tests

--- packages/pinball_flame/lib/src/contact_behavior.dart
+++ packages/pinball_flame/lib/src/contact_behavior.dart
@@ -3,19 +3,21 @@ import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 import 'package:pinball_flame/pinball_flame.dart';
 
-/// Adds a new [ContactCallbacks] to the parent.
+/// Appends a new [ContactCallbacks] to the parent.
 ///
 /// This is a convenience class for adding a [ContactCallbacks] to the parent.
-/// In constract with just adding a [ContactCallbacks] to the parent's userData,
-/// this class respects the previous [ContactCallbacks] in the parent's
-/// userData, if any. Hence, it avoids overriding any previous
+/// In constract with just adding a [ContactCallbacks] to the parent's body
+/// userData, this class respects the previous [ContactCallbacks] in the
+/// parent's body userData, if any. Hence, it avoids overriding any previous
 /// [ContactCallbacks] in the parent.
 ///
 /// It does so by grouping the [ContactCallbacks] in a [_ContactCallbacksGroup],
 /// and resetting the parent's userData accordingly.
 // TODO(alestiago): Make use of generics to infer the type of the contact.
 // https://github.com/VGVentures/pinball/pull/234#discussion_r859182267
-abstract class ContactBehavior<T extends BodyComponent> extends Component
+// TODO(alestiago): Consider if there is a need to support adjusting a fixture's
+// userData.
+class ContactBehavior<T extends BodyComponent> extends Component
     with ContactCallbacks, ParentIsA<T> {
   @override
   @mustCallSuper
