4ddce9550cab78f983cd457b63ab94dd3dcd47a2
arturplaczek <arturp1989@gmail.com>
2022-04-28 17:47:01 +0200

    chore: create StarAnimation

---
+++ test/select_character/widgets/star_animation_test.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: invalid_use_of_protected_member
+
+import 'package:flame/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pinball/select_character/select_character.dart';
+
+import '../../helpers/helpers.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('loads SpriteAnimationWidget correctly for', () {
+    setUpAll(() async {
+      await StarAnimation.loadAssets();
+    });
+
+    testWidgets('starA', (tester) async {
+      await tester.pumpApp(
+        StarAnimation.starA(),
+      );
+      await tester.pump();
+
+      expect(find.byType(SpriteAnimationWidget), findsOneWidget);
+    });
+
+    testWidgets('starB', (tester) async {
+      await tester.pumpApp(
+        StarAnimation.starB(),
+      );
+      await tester.pump();
+
+      expect(find.byType(SpriteAnimationWidget), findsOneWidget);
+    });
+
+    testWidgets('starC', (tester) async {
+      await tester.pumpApp(
+        StarAnimation.starC(),
+      );
+      await tester.pump();
+
+      expect(find.byType(SpriteAnimationWidget), findsOneWidget);
+    });
+  });
+}
