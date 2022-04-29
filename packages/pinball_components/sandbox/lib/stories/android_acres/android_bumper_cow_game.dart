bf95b16772c9b05cbfecfb6be27b4e788bf41921
RuiAlonso <rui.alonso@verygood.ventures>
2022-04-29 17:01:10 +0200

    feat: cow sanbox

---
+++ packages/pinball_components/sandbox/lib/stories/android_acres/android_bumper_cow_game.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flame/extensions.dart';
+import 'package:pinball_components/pinball_components.dart';
+import 'package:sandbox/stories/ball/basic_ball_game.dart';
+
+class AndroidBumperCowGame extends BallGame {
+  AndroidBumperCowGame()
+      : super(
+          color: const Color(0xFF0000FF),
+          imagesFileNames: [
+            Assets.images.androidBumper.cow.lit.keyName,
+            Assets.images.androidBumper.cow.dimmed.keyName,
+          ],
+        );
+
+  static const description = '''
+    Shows how a AndroidBumperCowGame is rendered.
+
+    - Activate the "trace" parameter to overlay the body.
+''';
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    camera.followVector2(Vector2.zero());
+    await add(
+      AndroidBumper.cow()..priority = 1,
+    );
+
+    await traceAllBodies();
+  }
+}
