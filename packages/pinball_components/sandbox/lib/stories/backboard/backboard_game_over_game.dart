a563d6a20dca862a4e43d097d7ef78260eba1da4
RuiAlonso <rui.alonso@verygood.ventures>
2022-04-30 23:03:54 +0200

    refactor: refactored sandbox for scoretexts

--- packages/pinball_components/sandbox/lib/stories/backboard/backboard_game_over_game.dart
+++ packages/pinball_components/sandbox/lib/stories/backboard/backboard_game_over_game.dart
@@ -1,6 +1,6 @@
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
-import 'package:pinball_components/pinball_components.dart' hide Assets;
+import 'package:pinball_components/pinball_components.dart' as components;
 import 'package:pinball_theme/pinball_theme.dart';
 import 'package:sandbox/common/common.dart';
 
@@ -8,7 +8,32 @@ class BackboardGameOverGame extends AssetsGame
     with HasKeyboardHandlerComponents {
   BackboardGameOverGame(this.score, this.character)
       : super(
-          imagesFileNames: characterIconPaths.values.toList(),
+          imagesFileNames: [
+            components.Assets.images.score.points5k.keyName,
+            components.Assets.images.score.points10k.keyName,
+            components.Assets.images.score.points15k.keyName,
+            components.Assets.images.score.points20k.keyName,
+            components.Assets.images.score.points25k.keyName,
+            components.Assets.images.score.points30k.keyName,
+            components.Assets.images.score.points40k.keyName,
+            components.Assets.images.score.points50k.keyName,
+            components.Assets.images.score.points60k.keyName,
+            components.Assets.images.score.points80k.keyName,
+            components.Assets.images.score.points100k.keyName,
+            components.Assets.images.score.points120k.keyName,
+            components.Assets.images.score.points200k.keyName,
+            components.Assets.images.score.points400k.keyName,
+            components.Assets.images.score.points600k.keyName,
+            components.Assets.images.score.points800k.keyName,
+            components.Assets.images.score.points1m.keyName,
+            components.Assets.images.score.points1m2.keyName,
+            components.Assets.images.score.points2m.keyName,
+            components.Assets.images.score.points3m.keyName,
+            components.Assets.images.score.points4m.keyName,
+            components.Assets.images.score.points5m.keyName,
+            components.Assets.images.score.points6m.keyName,
+            ...characterIconPaths.values.toList(),
+          ],
         );
 
   static const description = '''
@@ -30,22 +55,23 @@ class BackboardGameOverGame extends AssetsGame
 
   @override
   Future<void> onLoad() async {
+    await super.onLoad();
+
     camera
       ..followVector2(Vector2.zero())
       ..zoom = 5;
 
     await add(
-      Backboard.gameOver(
+      components.Backboard.gameOver(
         position: Vector2(0, 20),
         score: score,
         characterIconPath: characterIconPaths[character]!,
         onSubmit: (initials) {
           add(
-            ScoreText(
-              score: Score.points_6m,
-              //text: 'User $initials made $score',
+            components.ScoreComponent(
+              score: components.Score.values
+                  .firstWhere((element) => element.value == score),
               position: Vector2(0, 50),
-              //color: Colors.pink,
             ),
           );
         },
---
+++ packages/pinball_components/sandbox/lib/stories/score/score_game.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flame/input.dart';
+import 'package:pinball_components/pinball_components.dart';
+import 'package:sandbox/common/common.dart';
+
+class ScoreGame extends AssetsGame with TapDetector {
+  ScoreGame()
+      : super(
+          imagesFileNames: [
+            Assets.images.score.points1m.keyName,
+            Assets.images.score.points1m2.keyName,
+            Assets.images.score.points2m.keyName,
+            Assets.images.score.points3m.keyName,
+            Assets.images.score.points4m.keyName,
+            Assets.images.score.points5k.keyName,
+            Assets.images.score.points5m.keyName,
+            Assets.images.score.points6m.keyName,
+            Assets.images.score.points10k.keyName,
+            Assets.images.score.points15k.keyName,
+            Assets.images.score.points20k.keyName,
+            Assets.images.score.points25k.keyName,
+            Assets.images.score.points30k.keyName,
+            Assets.images.score.points40k.keyName,
+            Assets.images.score.points50k.keyName,
+            Assets.images.score.points60k.keyName,
+            Assets.images.score.points80k.keyName,
+            Assets.images.score.points100k.keyName,
+            Assets.images.score.points120k.keyName,
+            Assets.images.score.points200k.keyName,
+            Assets.images.score.points400k.keyName,
+            Assets.images.score.points600k.keyName,
+            Assets.images.score.points800k.keyName,
+          ],
+        );
+
+  static const description = '''
+    Simple game to show how score component works,
+
+    - Tap anywhere on the screen to spawn an image on the given location.
+''';
+
+  final random = Random();
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    camera.followVector2(Vector2.zero());
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    final index = random.nextInt(Score.values.length);
+    final score = Score.values[index];
+
+    add(
+      ScoreComponent(
+        score: score,
+        position: info.eventPosition.game..multiply(Vector2(1, -1)),
+      ),
+    );
+  }
+}
