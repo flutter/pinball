c2904652479d3bc424ed9e078d79b6e5a6417281
Jorge Coca <jorgecoca@users.noreply.github.com>
2022-04-29 20:28:21 -0500

    refactor(leaderboard_repository): only keep the top-10 score in the leaderboard collection (#265)

--- packages/leaderboard_repository/lib/src/leaderboard_repository.dart
+++ packages/leaderboard_repository/lib/src/leaderboard_repository.dart
@@ -1,91 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:leaderboard_repository/leaderboard_repository.dart';
 
-/// {@template leaderboard_exception}
-/// Base exception for leaderboard repository failures.
-/// {@endtemplate}
-abstract class LeaderboardException implements Exception {
-  /// {@macro leaderboard_exception}
-  const LeaderboardException(this.error, this.stackTrace);
-
-  /// The error that was caught.
-  final Object error;
-
-  /// The Stacktrace associated with the [error].
-  final StackTrace stackTrace;
-}
-
-/// {@template leaderboard_deserialization_exception}
-/// Exception thrown when leaderboard data cannot be deserialized in the
-/// expected way.
-/// {@endtemplate}
-class LeaderboardDeserializationException extends LeaderboardException {
-  /// {@macro leaderboard_deserialization_exception}
-  const LeaderboardDeserializationException(
-    Object error,
-    StackTrace stackTrace,
-  ) : super(
-          error,
-          stackTrace,
-        );
-}
-
-/// {@template fetch_top_10_leaderboard_exception}
-/// Exception thrown when failure occurs while fetching top 10 leaderboard.
-/// {@endtemplate}
-class FetchTop10LeaderboardException extends LeaderboardException {
-  /// {@macro fetch_top_10_leaderboard_exception}
-  const FetchTop10LeaderboardException(
-    Object error,
-    StackTrace stackTrace,
-  ) : super(
-          error,
-          stackTrace,
-        );
-}
-
-/// {@template add_leaderboard_entry_exception}
-/// Exception thrown when failure occurs while adding entry to leaderboard.
-/// {@endtemplate}
-class AddLeaderboardEntryException extends LeaderboardException {
-  /// {@macro add_leaderboard_entry_exception}
-  const AddLeaderboardEntryException(
-    Object error,
-    StackTrace stackTrace,
-  ) : super(
-          error,
-          stackTrace,
-        );
-}
-
-/// {@template fetch_player_ranking_exception}
-/// Exception thrown when failure occurs while fetching player ranking.
-/// {@endtemplate}
-class FetchPlayerRankingException extends LeaderboardException {
-  /// {@macro fetch_player_ranking_exception}
-  const FetchPlayerRankingException(
-    Object error,
-    StackTrace stackTrace,
-  ) : super(
-          error,
-          stackTrace,
-        );
-}
-
-/// {@template fetch_prohibited_initials_exception}
-/// Exception thrown when failure occurs while fetching prohibited initials.
-/// {@endtemplate}
-class FetchProhibitedInitialsException extends LeaderboardException {
-  /// {@macro fetch_prohibited_initials_exception}
-  const FetchProhibitedInitialsException(
-    Object error,
-    StackTrace stackTrace,
-  ) : super(
-          error,
-          stackTrace,
-        );
-}
-
 /// {@template leaderboard_repository}
 /// Repository to access leaderboard data in Firebase Cloud Firestore.
 /// {@endtemplate}
@@ -97,73 +12,40 @@ class LeaderboardRepository {
 
   final FirebaseFirestore _firebaseFirestore;
 
+  static const _leaderboardLimit = 10;
+  static const _leaderboardCollectionName = 'leaderboard';
+  static const _scoreFieldName = 'score';
+
   /// Acquires top 10 [LeaderboardEntryData]s.
   Future<List<LeaderboardEntryData>> fetchTop10Leaderboard() async {
-    final leaderboardEntries = <LeaderboardEntryData>[];
-    late List<QueryDocumentSnapshot> documents;
-
     try {
       final querySnapshot = await _firebaseFirestore
-          .collection('leaderboard')
-          .orderBy('score', descending: true)
-          .limit(10)
+          .collection(_leaderboardCollectionName)
+          .orderBy(_scoreFieldName, descending: true)
+          .limit(_leaderboardLimit)
           .get();
-      documents = querySnapshot.docs;
+      final documents = querySnapshot.docs;
+      return documents.toLeaderboard();
+    } on LeaderboardDeserializationException {
+      rethrow;
     } on Exception catch (error, stackTrace) {
       throw FetchTop10LeaderboardException(error, stackTrace);
     }
-
-    for (final document in documents) {
-      final data = document.data() as Map<String, dynamic>?;
-      if (data != null) {
-        try {
-          leaderboardEntries.add(LeaderboardEntryData.fromJson(data));
-        } catch (error, stackTrace) {
-          throw LeaderboardDeserializationException(error, stackTrace);
-        }
-      }
-    }
-
-    return leaderboardEntries;
   }
 
-  /// Adds player's score entry to the leaderboard and gets their
-  /// [LeaderboardRanking].
-  Future<LeaderboardRanking> addLeaderboardEntry(
+  /// Adds player's score entry to the leaderboard if it is within the top-10
+  Future<void> addLeaderboardEntry(
     LeaderboardEntryData entry,
   ) async {
-    late DocumentReference entryReference;
-    try {
-      entryReference = await _firebaseFirestore
-          .collection('leaderboard')
-          .add(entry.toJson());
-    } on Exception catch (error, stackTrace) {
-      throw AddLeaderboardEntryException(error, stackTrace);
-    }
-
-    try {
-      final querySnapshot = await _firebaseFirestore
-          .collection('leaderboard')
-          .orderBy('score', descending: true)
-          .get();
-
-      // TODO(allisonryan0002): see if we can find a more performant solution.
-      final documents = querySnapshot.docs;
-      final ranking = documents.indexWhere(
-            (document) => document.id == entryReference.id,
-          ) +
-          1;
-
-      if (ranking > 0) {
-        return LeaderboardRanking(ranking: ranking, outOf: documents.length);
-      } else {
-        throw FetchPlayerRankingException(
-          'Player score could not be found and ranking cannot be provided.',
-          StackTrace.current,
-        );
+    final leaderboard = await _fetchLeaderboardSortedByScore();
+    if (leaderboard.length < 10) {
+      await _saveScore(entry);
+    } else {
+      final tenthPositionScore = leaderboard[9].score;
+      if (entry.score > tenthPositionScore) {
+        await _saveScore(entry);
+        await _deleteScoresUnder(tenthPositionScore);
       }
-    } on Exception catch (error, stackTrace) {
-      throw FetchPlayerRankingException(error, stackTrace);
     }
   }
 
@@ -174,7 +56,6 @@ class LeaderboardRepository {
     if (!initialsRegex.hasMatch(initials)) {
       return false;
     }
-
     try {
       final document = await _firebaseFirestore
           .collection('prohibitedInitials')
@@ -186,5 +67,62 @@ class LeaderboardRepository {
     } on Exception catch (error, stackTrace) {
       throw FetchProhibitedInitialsException(error, stackTrace);
     }
+  }
+
+  Future<List<LeaderboardEntryData>> _fetchLeaderboardSortedByScore() async {
+    try {
+      final querySnapshot = await _firebaseFirestore
+          .collection(_leaderboardCollectionName)
+          .orderBy(_scoreFieldName, descending: true)
+          .get();
+      final documents = querySnapshot.docs;
+      return documents.toLeaderboard();
+    } on Exception catch (error, stackTrace) {
+      throw FetchLeaderboardException(error, stackTrace);
+    }
+  }
+
+  Future<void> _saveScore(LeaderboardEntryData entry) {
+    try {
+      return _firebaseFirestore
+          .collection(_leaderboardCollectionName)
+          .add(entry.toJson());
+    } on Exception catch (error, stackTrace) {
+      throw AddLeaderboardEntryException(error, stackTrace);
+    }
+  }
+
+  Future<void> _deleteScoresUnder(int score) async {
+    try {
+      final querySnapshot = await _firebaseFirestore
+          .collection(_leaderboardCollectionName)
+          .where(_scoreFieldName, isLessThanOrEqualTo: score)
+          .get();
+      final documents = querySnapshot.docs;
+      for (final document in documents) {
+        await document.reference.delete();
+      }
+    } on LeaderboardDeserializationException {
+      rethrow;
+    } on Exception catch (error, stackTrace) {
+      throw DeleteLeaderboardException(error, stackTrace);
+    }
+  }
+}
+
+extension on List<QueryDocumentSnapshot> {
+  List<LeaderboardEntryData> toLeaderboard() {
+    final leaderboardEntries = <LeaderboardEntryData>[];
+    for (final document in this) {
+      final data = document.data() as Map<String, dynamic>?;
+      if (data != null) {
+        try {
+          leaderboardEntries.add(LeaderboardEntryData.fromJson(data));
+        } catch (error, stackTrace) {
+          throw LeaderboardDeserializationException(error, stackTrace);
+        }
+      }
+    }
+    return leaderboardEntries;
   }
 }
