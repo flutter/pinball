c7e5cd05f4e7b9514e9d01595f81cf0a9feb892b
Jorge Coca <jorge.coca.github@gmail.com>
2022-04-30 13:37:46 -0500

    fix

--- packages/pinball_ui/test/src/external_links/external_links_test.dart
+++ packages/pinball_ui/test/src/external_links/external_links_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:pinball_ui/pinball_ui.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
 class MockUrlLauncher extends Mock
     with MockPlatformInterfaceMixin
