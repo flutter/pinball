d466070868d4a6b50046054f496a2a4405b3df78
RuiAlonso <rui.alonso@verygood.ventures>
2022-04-30 23:04:09 +0200

    refactor: moved score_text to score_component

--- packages/pinball_components/lib/src/components/components.dart
+++ packages/pinball_components/lib/src/components/components.dart
@@ -25,7 +25,7 @@ export 'multiplier/multiplier.dart';
 export 'plunger.dart';
 export 'render_priority.dart';
 export 'rocket.dart';
-export 'score_text.dart';
+export 'score_component.dart';
 export 'shapes/shapes.dart';
 export 'signpost.dart';
 export 'slingshot.dart';
