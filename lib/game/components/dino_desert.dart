02b40a0e567d26abc4c4f1c640303fe17551e2ca
Allison Ryan <allisonryan0002@gmail.com>
2022-05-01 10:32:18 -0500

    feat: add invisible barrier behind dino

--- lib/game/components/dino_desert.dart
+++ lib/game/components/dino_desert.dart
@@ -1,4 +1,4 @@
-import 'package:flame/components.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:pinball/game/game.dart';
 import 'package:pinball_components/pinball_components.dart';
 import 'package:pinball_flame/pinball_flame.dart';
@@ -17,9 +17,23 @@ class DinoDesert extends Blueprint {
                 ScoringBehavior(points: 200000)..applyTo(['inside_mouth']),
               ],
             )..initialPosition = Vector2(12.3, -6.9),
+            _BarrierBehindDino(),
           ],
           blueprints: [
             DinoWalls(),
           ],
         );
+}
+
+class _BarrierBehindDino extends BodyComponent {
+  @override
+  Body createBody() {
+    final shape = EdgeShape()
+      ..set(
+        Vector2(27, -14.2),
+        Vector2(25, -7.7),
+      );
+
+    return world.createBody(BodyDef())..createFixtureFromShape(shape);
+  }
 }
