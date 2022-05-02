f2160feab90d7b738caca196ab4420b5aa371474
Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>
2022-05-02 17:41:39 -0500

    feat: implemented `AndroidSpaceshipBonusBehavior` (#298)
    * refactor: simplify ball entering spaceship
    
    * feat: add bonus logic and scoring
    
    * chore: add animatronic to spaceship sandbox
    
    * chore: remove renderBody change
    
    * chore: remove leftover children change
    
    * chore: test typo
    
    * refactor: re-add children property
    
    * refactor: use firstChild
    
    * refactor: PR suggestions
    
    * Update packages/pinball_components/test/src/components/android_animatronic_test.dart
    
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>
    
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

---
+++ packages/pinball_components/lib/src/components/android_spaceship/behaviors/android_spaceship_entrance_ball_contact_behavior.dart.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:pinball_components/pinball_components.dart';
+import 'package:pinball_flame/pinball_flame.dart';
+
+class AndroidSpaceshipEntranceBallContactBehavior
+    extends ContactBehavior<AndroidSpaceshipEntrance> {
+  @override
+  void beginContact(Object other, Contact contact) {
+    super.beginContact(other, contact);
+    if (other is! Ball) return;
+
+    parent.parent.bloc.onBallEntered();
+  }
+}
