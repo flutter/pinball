45450f1f0234b42c4af8552a1fc8374484380fc7
Tom Arra <tarra3@gmail.com>
2022-05-02 10:05:26 -0500

    chore: remove VGV copyright

--- packages/pinball_components/sandbox/lib/main.dart
+++ packages/pinball_components/sandbox/lib/main.dart
@@ -1,9 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
 import 'package:dashbook/dashbook.dart';
 import 'package:flutter/material.dart';
 import 'package:sandbox/stories/stories.dart';
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leaderboard_repository/leaderboard_repository.dart';
