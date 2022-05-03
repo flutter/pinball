7c05ad096cf941bcc8469622fb00dd7f31ac2a25
Tom Arra <tarra3@gmail.com>
2022-05-03 14:02:58 -0500

    Revert "first config changes for release"
    This reverts commit 40104d037dd0bd92d4a23a7962c640f4c6dd8445.

---
+++ web/__/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBgMVAvYccjNypCDdpW0ol6syCcISU2yjM",
+  "appId": "1:725488140557:web:7c61a0755fc23436fe7044",
+  "authDomain": "pinball-dev.firebaseapp.com",
+  "databaseURL": "",
+  "measurementId": "G-9NW8SZRFJR",
+  "messagingSenderId": "725488140557",
+  "projectId": "pinball-dev",
+  "storageBucket": "pinball-dev.appspot.com"
+});
