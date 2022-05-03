f42927736ef3a61b4a680af59a88b0a40ad20605
alestiago <dev@alestiago.com>
2022-05-03 17:20:46 +0100

    feat: defined BallScalingBehavior

---
+++ packages/pinball_components/lib/src/components/ball/behaviors/ball_scaling_behavior.dart
@@ -0,0 +1,21 @@
+import 'package:flame/components.dart';
+import 'package:pinball_components/pinball_components.dart';
+import 'package:pinball_flame/pinball_flame.dart';
+
+/// Scales the ball body and sprite accordingly to the board's perspective.
+class BallScalingBehavior extends Component with ParentIsA<Ball> {
+  @override
+  void update(double dt) {
+    super.update(dt);
+    final boardHeight = BoardDimensions.bounds.height;
+    const maxShrinkValue = BoardDimensions.perspectiveShrinkFactor;
+
+    final standardizedYPosition = parent.body.position.y + (boardHeight / 2);
+    final scaleFactor = maxShrinkValue +
+        ((standardizedYPosition / boardHeight) * (1 - maxShrinkValue));
+
+    parent.body.fixtures.first.shape.radius = (Ball.size.x / 2) * scaleFactor;
+
+    parent.firstChild<SpriteComponent>()!.scale = Vector2.all(scaleFactor);
+  }
+}
