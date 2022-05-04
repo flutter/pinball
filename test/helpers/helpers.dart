6c64af269c5396c44366c94c77a2c2be98e34566
arturplaczek <arturp1989@gmail.com>
2022-05-04 15:41:43 +0200

    fix: mock flame images

--- test/helpers/helpers.dart
+++ test/helpers/helpers.dart
@@ -2,6 +2,7 @@ export 'builders.dart';
 export 'fakes.dart';
 export 'forge2d.dart';
 export 'key_testers.dart';
+export 'mock_flame_images.dart';
 export 'pump_app.dart';
 export 'test_games.dart';
 export 'text_span.dart';
---
+++ test/helpers/mock_flame_images.dart
@@ -0,0 +1,86 @@
+import 'dart:typed_data';
+
+import 'package:flame/assets.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _MockImages extends Mock implements Images {}
+
+Future<void> mockFlameImages() async {
+  // TODO(arturplaczek): need to find for a better solution for loading image
+  // or use original images.
+  final image = await decodeImageFromList(Uint8List.fromList(fakeImage));
+  final images = _MockImages();
+  when(() => images.fromCache(any())).thenReturn(image);
+  when(() => images.load(any())).thenAnswer((_) => Future.value(image));
+  Flame.images = images;
+}
+
+const fakeImage = <int>[
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
+];
