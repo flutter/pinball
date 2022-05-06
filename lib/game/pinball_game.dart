481851b51f3f0433f41d6ad88926466e7f6ce92b
Allison Ryan <allisonryan0002@gmail.com>
2022-05-06 12:46:37 -0500

    refactor: sparky behaviors and remove controlled ball

--- lib/game/pinball_game.dart
+++ lib/game/pinball_game.dart
@@ -187,8 +187,7 @@ class DebugPinballGame extends PinballGame with FPSCounter, PanDetector {
 
     if (info.raw.kind == PointerDeviceKind.mouse) {
       final canvas = descendants().whereType<ZCanvasComponent>().single;
-      final ball = ControlledBall.debug()
-        ..initialPosition = info.eventPosition.game;
+      final ball = Ball()..initialPosition = info.eventPosition.game;
       canvas.add(ball);
     }
   }
@@ -215,7 +214,7 @@ class DebugPinballGame extends PinballGame with FPSCounter, PanDetector {
 
   void _turboChargeBall(Vector2 line) {
     final canvas = descendants().whereType<ZCanvasComponent>().single;
-    final ball = ControlledBall.debug()..initialPosition = lineStart!;
+    final ball = Ball()..initialPosition = lineStart!;
     final impulse = line * -1 * 10;
     ball.add(BallTurboChargingBehavior(impulse: impulse));
     canvas.add(ball);
@@ -261,7 +260,7 @@ class _DebugInformation extends Component with HasGameRef<DebugPinballGame> {
   void render(Canvas canvas) {
     final debugText = [
       'FPS: ${gameRef.fps().toStringAsFixed(1)}',
-      'BALLS: ${gameRef.descendants().whereType<ControlledBall>().length}',
+      'BALLS: ${gameRef.descendants().whereType<Ball>().length}',
     ].join(' | ');
 
     final height = _debugTextPaint.measureTextHeight(debugText);
---
+++ packages/pinball_components/lib/src/components/sparky_computer/behaviors/sparky_computer_sensor_ball_contact_behavior.dart
@@ -0,0 +1,36 @@
+import 'package:flame/components.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:pinball_components/pinball_components.dart';
+import 'package:pinball_flame/pinball_flame.dart';
+
+/// {@template sparky_computer_sensor_ball_contact_behavior}
+/// When a [Ball] enters the [SparkyComputer] it is stopped for a period of time
+/// before a [BallTurboChargingBehavior] is applied to it.
+/// {@endtemplate}
+class SparkyComputerSensorBallContactBehavior
+    extends ContactBehavior<SparkyComputer> {
+  @override
+  Future<void> beginContact(Object other, Contact contact) async {
+    super.beginContact(other, contact);
+    if (other is! Ball) return;
+
+    other.stop();
+    parent.bloc.onBallEntered();
+    await parent.add(
+      TimerComponent(
+        period: 1.5,
+        onTick: () {
+          other
+            ..resume()
+            ..add(
+              BallTurboChargingBehavior(
+                impulse: Vector2(40, 110),
+              ),
+            );
+          parent.bloc.onBallTurboCharged();
+        },
+        removeOnFinish: true,
+      ),
+    );
+  }
+}
