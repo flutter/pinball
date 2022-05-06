ad8276bfc131782edb117d1aef117e1780701743
alestiago <dev@alestiago.com>
2022-05-06 04:43:06 +0100

    refactor: removed unused files

--- test/helpers/fakes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flame_forge2d/flame_forge2d.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:pinball/game/game.dart';
-
-class FakeContact extends Fake implements Contact {}
-
-class FakeGameEvent extends Fake implements GameEvent {}
--- test/helpers/helpers.dart
+++ test/helpers/helpers.dart
@@ -1,6 +1,4 @@
 export 'builders.dart';
-export 'fakes.dart';
-export 'forge2d.dart';
 export 'key_testers.dart';
 export 'mock_flame_images.dart';
 export 'pump_app.dart';
