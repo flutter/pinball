47666efa4f59c932992796a581d67aa218d132d6
alestiago <dev@alestiago.com>
2022-05-06 05:12:39 +0100

    refactor: removed unecessary imports

--- test/game/components/backbox/displays/initials_submission_failure_display_test.dart
+++ test/game/components/backbox/displays/initials_submission_failure_display_test.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: cascade_invocations
 
 import 'package:flame/components.dart';
-import 'package:flame/input.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
