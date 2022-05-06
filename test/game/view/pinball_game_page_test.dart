2e1cc0a335de1e5f1815ff38d07ec0813911844e
Felix Angelov <felangelov@gmail.com>
2022-05-06 16:05:30 -0500

    refactor: provide GameBloc in App

--- test/game/view/pinball_game_page_test.dart
+++ test/game/view/pinball_game_page_test.dart
@@ -82,6 +82,7 @@ void main() {
       await tester.pumpApp(
         PinballGamePage(),
         characterThemeCubit: characterThemeCubit,
+        gameBloc: gameBloc,
       );
 
       expect(find.byType(PinballGameView), findsOneWidget);
@@ -170,6 +171,7 @@ void main() {
             ),
           ),
           characterThemeCubit: characterThemeCubit,
+          gameBloc: gameBloc,
         );
 
         await tester.tap(find.text('Tap me'));
