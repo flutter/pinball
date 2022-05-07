78a2b0438a1351ea1fb8577a1edc1e4b5c8148cb
alestiago <dev@alestiago.com>
2022-05-07 12:54:44 +0100

    refactor: moved z_index_contact_behavior to pinball_flame

--- packages/pinball_components/lib/src/components/spaceship_ramp/behavior/behavior.dart
+++ packages/pinball_components/lib/src/components/spaceship_ramp/behavior/behavior.dart
@@ -1,3 +1,2 @@
 export 'layer_contact_behavior.dart';
 export 'ramp_ball_ascending_contact_behavior.dart';
-export 'z_index_contact_behavior.dart';
--- packages/pinball_flame/lib/src/behaviors/behaviors.dart
+++ packages/pinball_flame/lib/src/behaviors/behaviors.dart
@@ -1,1 +1,2 @@
 export 'contact_behavior.dart';
+export 'z_index_contact_behavior.dart';
