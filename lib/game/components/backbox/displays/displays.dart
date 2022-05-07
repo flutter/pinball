0b7a1cfac1431bb73cd0f699ae70fe1b990824df
Erick <erickzanardoo@gmail.com>
2022-05-07 18:23:41 -0300

    feat: adding leaderboard error screen (#398)
    * feat: adding leaderboard error screen
    
    * Update lib/game/components/backbox/displays/leaderboard_failure_display.dart
    
    Co-authored-by: Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>
    
    Co-authored-by: Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>

--- lib/game/components/backbox/displays/displays.dart
+++ lib/game/components/backbox/displays/displays.dart
@@ -2,4 +2,5 @@ export 'initials_input_display.dart';
 export 'initials_submission_failure_display.dart';
 export 'initials_submission_success_display.dart';
 export 'leaderboard_display.dart';
+export 'leaderboard_failure_display.dart';
 export 'loading_display.dart';
