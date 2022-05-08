21bf4f478d85c0c8a8939bdb19f6049fd587245d
Alejandro Santiago <dev@alestiago.com>
2022-05-08 01:06:41 +0100

    refactor: removed FireEffect (#402)

--- packages/pinball_components/sandbox/lib/stories/effects/stories.dart
+++ packages/pinball_components/sandbox/lib/stories/effects/stories.dart
@@ -1,18 +1,11 @@
 import 'package:dashbook/dashbook.dart';
 import 'package:sandbox/common/common.dart';
 import 'package:sandbox/stories/effects/camera_zoom_game.dart';
-import 'package:sandbox/stories/effects/fire_effect_game.dart';
 
 void addEffectsStories(Dashbook dashbook) {
-  dashbook.storiesOf('Effects')
-    ..addGame(
-      title: 'Fire',
-      description: FireEffectGame.description,
-      gameBuilder: (_) => FireEffectGame(),
-    )
-    ..addGame(
-      title: 'CameraZoom',
-      description: CameraZoomGame.description,
-      gameBuilder: (_) => CameraZoomGame(),
-    );
+  dashbook.storiesOf('Effects').addGame(
+        title: 'CameraZoom',
+        description: CameraZoomGame.description,
+        gameBuilder: (_) => CameraZoomGame(),
+      );
 }
