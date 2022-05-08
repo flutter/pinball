cddafc7fe040ee587ad795f46cc92cf80e8dcc91
alestiago <dev@alestiago.com>
2022-05-08 11:33:12 +0100

    refactor: simplified BezierCurveShape test

--- packages/pinball_flame/test/src/shapes/bezier_curve_shape_test.dart
+++ packages/pinball_flame/test/src/shapes/bezier_curve_shape_test.dart
@@ -5,21 +5,15 @@ import 'package:pinball_flame/pinball_flame.dart';
 
 void main() {
   group('BezierCurveShape', () {
-    late List<Vector2> controlPoints;
-
-    setUp(() {
-      controlPoints = [
-        Vector2(0, 0),
-        Vector2(10, 0),
-        Vector2(0, 10),
-        Vector2(10, 10),
-      ];
-    });
-
     test('can be instantiated', () {
       expect(
         BezierCurveShape(
-          controlPoints: controlPoints,
+          controlPoints: [
+            Vector2(0, 0),
+            Vector2(10, 0),
+            Vector2(0, 10),
+            Vector2(10, 10),
+          ],
         ),
         isA<BezierCurveShape>,
       );
