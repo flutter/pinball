8932be9097427dc3807e092d7c3f10e09fc6308b
Allison Ryan <allisonryan0002@gmail.com>
2022-05-09 14:07:09 -0500

    chore: fix sandbox and asset cache loading

--- packages/pinball_components/sandbox/lib/stories/plunger/plunger_game.dart
+++ packages/pinball_components/sandbox/lib/stories/plunger/plunger_game.dart
@@ -1,10 +1,18 @@
 import 'package:flame/input.dart';
+import 'package:flame_bloc/flame_bloc.dart';
 import 'package:pinball_components/pinball_components.dart';
 import 'package:sandbox/common/common.dart';
 import 'package:sandbox/stories/ball/basic_ball_game.dart';
 
 class PlungerGame extends BallGame
     with HasKeyboardHandlerComponents, Traceable {
+  PlungerGame()
+      : super(
+          imagesFileNames: [
+            Assets.images.plunger.plunger.keyName,
+          ],
+        );
+
   static const description = '''
     Shows how Plunger is rendered.
 
@@ -19,7 +27,12 @@ class PlungerGame extends BallGame
     final center = screenToWorld(camera.viewport.canvasSize! / 2);
     final plunger = Plunger()
       ..initialPosition = Vector2(center.x - 8.8, center.y);
-    await add(plunger);
+    await add(
+      FlameBlocProvider<PlungerCubit, PlungerState>(
+        create: PlungerCubit.new,
+        children: [plunger],
+      ),
+    );
     await plunger.add(PlungerKeyControllingBehavior());
 
     await traceAllBodies();
