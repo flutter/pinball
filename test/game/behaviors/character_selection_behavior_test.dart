97b162033a0fd3269518e8582d9e269487e8de00
Allison Ryan <allisonryan0002@gmail.com>
2022-05-10 09:30:58 -0500

    fix: ball theming on mobile

--- test/game/behaviors/character_selection_behavior_test.dart
+++ test/game/behaviors/character_selection_behavior_test.dart
@@ -77,8 +77,45 @@ void main() {
       );
 
       flameTester.test(
-        'onNewState calls onCharacterSelected on the arcade background bloc',
+        'onNewState does not call onCharacterSelected on the arcade background '
+        'bloc when platform is mobile',
         (game) async {
+          final platformHelper = _MockPlatformHelper();
+          when(() => platformHelper.isMobile).thenAnswer((_) => true);
+          final arcadeBackgroundBloc = _MockArcadeBackgroundCubit();
+          whenListen(
+            arcadeBackgroundBloc,
+            const Stream<ArcadeBackgroundState>.empty(),
+            initialState: const ArcadeBackgroundState.initial(),
+          );
+          final behavior = CharacterSelectionBehavior();
+          await game.pump(
+            [
+              behavior,
+              ZCanvasComponent(),
+              Plunger.test(),
+              Ball.test(),
+            ],
+            platformHelper: platformHelper,
+          );
+
+          const dinoThemeState = CharacterThemeState(theme.DinoTheme());
+          behavior.onNewState(dinoThemeState);
+          await game.ready();
+
+          verifyNever(
+            () => arcadeBackgroundBloc
+                .onCharacterSelected(dinoThemeState.characterTheme),
+          );
+        },
+      );
+
+      flameTester.test(
+        'onNewState calls onCharacterSelected on the arcade background '
+        'bloc when platform is not mobile',
+        (game) async {
+          final platformHelper = _MockPlatformHelper();
+          when(() => platformHelper.isMobile).thenAnswer((_) => false);
           final arcadeBackgroundBloc = _MockArcadeBackgroundCubit();
           whenListen(
             arcadeBackgroundBloc,
@@ -96,6 +133,7 @@ void main() {
               Plunger.test(),
               Ball.test(),
             ],
+            platformHelper: platformHelper,
           );
 
           const dinoThemeState = CharacterThemeState(theme.DinoTheme());
