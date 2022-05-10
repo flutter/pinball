86980a70353ea0d43b59f50352dc1cf5ae68a3f3
Allison Ryan <77211884+allisonryan0002@users.noreply.github.com>
2022-05-10 10:31:44 -0500

    fix: ball theming on mobile (#462)

--- lib/game/behaviors/character_selection_behavior.dart
+++ lib/game/behaviors/character_selection_behavior.dart
@@ -2,6 +2,8 @@ import 'package:flame/components.dart';
 import 'package:flame_bloc/flame_bloc.dart';
 import 'package:pinball/select_character/select_character.dart';
 import 'package:pinball_components/pinball_components.dart';
+import 'package:pinball_flame/pinball_flame.dart';
+import 'package:platform_helper/platform_helper.dart';
 
 /// Updates the [ArcadeBackground] and launch [Ball] to reflect character
 /// selections.
@@ -11,12 +13,14 @@ class CharacterSelectionBehavior extends Component
         HasGameRef {
   @override
   void onNewState(CharacterThemeState state) {
-    gameRef
-        .descendants()
-        .whereType<ArcadeBackground>()
-        .single
-        .bloc
-        .onCharacterSelected(state.characterTheme);
+    if (!readProvider<PlatformHelper>().isMobile) {
+      gameRef
+          .descendants()
+          .whereType<ArcadeBackground>()
+          .single
+          .bloc
+          .onCharacterSelected(state.characterTheme);
+    }
     gameRef
         .descendants()
         .whereType<Ball>()
