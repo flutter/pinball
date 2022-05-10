9255744c87b51d3c105848ee2af4899d9014bc1f
Allison Ryan <allisonryan0002@gmail.com>
2022-05-10 08:37:13 -0500

    docs: looping behavior

--- lib/game/behaviors/animatronic_looping_behavior.dart
+++ lib/game/behaviors/animatronic_looping_behavior.dart
@@ -1,8 +1,13 @@
 import 'package:flame/components.dart';
 import 'package:pinball_flame/pinball_flame.dart';
 
+/// {@template animatronic_looping_behavior}
+/// Behavior that can be added to a [SpriteAnimationComponent] to repeatedly
+/// trigger its animation at a set interval.
+/// {@endtemplate}
 class AnimatronicLoopingBehavior extends TimerComponent
     with ParentIsA<SpriteAnimationComponent> {
+  /// {@macro animatronic_looping_behavior}
   AnimatronicLoopingBehavior({
     required double animationCoolDown,
   }) : super(period: animationCoolDown);
